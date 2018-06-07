7996ec89288c28de7e71d211c563e39faaf58f09
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-06-07 11:08:32 +0300

    Trying to fix IE error

--- src/LandingHero/index.js
+++ src/LandingHero/index.js
@@ -15,15 +15,15 @@ export default ({}) => (
   <LandingHero>
     <About>
       <Title>
-        {/*<TextRotate*/}
-          {/*textBefore={`Open-source`}*/}
-          {/*words={[*/}
-            {/*`Version Control System`,*/}
-            {/*`Experiments Framework`,*/}
-            {/*`Deployment & Collaboration`*/}
-          {/*]}*/}
-          {/*textAfter={`for Data Science Projects.`}*/}
-        {/*/>*/}
+        <TextRotate
+          textBefore={`Open-source`}
+          words={[
+            `Version Control System`,
+            `Experiments Framework`,
+            `Deployment & Collaboration`
+          ]}
+          textAfter={`for Data Science Projects.`}
+        />
       </Title>
       <Buttons>
         <OnlyMobile>
