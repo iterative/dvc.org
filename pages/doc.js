cc9ad3efe88fcabd47bec145e14b680db312d927
Andrey Shelkovnikov <shelkov09@gmail.com>
2018-06-29 00:50:59 +0300

    documentation sidebar, smooth anchors

--- pages/doc.js
+++ pages/doc.js
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown'
 import Page from '../src/Page'
 import SearchForm from '../src/SearchForm'
 import DownloadButton from '../src/DownloadButton'
-// syntax highlight
+// code highlighter
 import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter/light'
 import docco from 'react-syntax-highlighter/styles/hljs/docco'
 import js from 'react-syntax-highlighter/languages/hljs/javascript'
@@ -12,13 +12,30 @@ import python from 'react-syntax-highlighter/languages/hljs/python'
 import bash from 'react-syntax-highlighter/languages/hljs/bash'
 // utils
 import fetch from 'isomorphic-fetch'
+import kebabCase from 'lodash.kebabcase'
+import { scroller, animateScroll } from 'react-scroll'
 // styles
 import styled from 'styled-components'
 import { media, OnlyDesktop } from '../src/styles'
+// documentation
+import sidebar from '../src/Documentation/sidebar'
 
 registerLanguage('js', js)
 registerLanguage('python', python)
 registerLanguage('sh', bash)
+
+function flatten(text, child) {
+  return typeof child === 'string'
+    ? text + child
+    : React.Children.toArray(child.props.children).reduce(flatten, text)
+}
+
+const HeadingRenderer = (props) => {
+  var children = React.Children.toArray(props.children)
+  var text = children.reduce(flatten, '')
+  var slug = kebabCase(text)
+  return React.createElement('h' + props.level, { id: slug }, props.children)
+}
 
 const CodeBlock = ({ value, language }) => (
   <SyntaxHighlighter
@@ -31,30 +48,58 @@ const CodeBlock = ({ value, language }) => (
 
 export default class Documentation extends Component {
   state = {
-    markdown: ''
+    activeSection: 0,
+    markdown: '',
   }
 
-  componentWillMount() {
-    if (process.browser) {
-      // fetch('/static/doc/markdown-test.md')
-      fetch('/static/docs/redux-saga.md')
-        .then(res => {
-          res.text().then(r => {
-            this.setState({ markdown: r })
-           })
+  componentDidMount() {
+    this.loadFile(sidebar[0].file)
+  }
+
+  selectSection = (index) => () => {
+    this.loadFile(sidebar[index].file)
+    this.setState({
+      activeSection: index
+    })
+  }
+
+  scrollToLink = (href) => () => {
+    scroller.scrollTo(href.slice(1), {
+      duration: 500,
+      offset: -85,
+      delay: 0,
+      smooth: 'easeInOut',
+    })
+  }
+
+  scrollTop = () => {
+    animateScroll.scrollTo(0, {
+      duration: 300,
+      offset: -85,
+      delay: 0,
+      smooth: 'easeInOut',
+    })
+  }
+
+  loadFile = (file) => {
+    fetch(file)
+      .then(res => {
+        res.text().then(r => {
+          this.setState({ markdown: r })
+          this.scrollTop()
         })
-    }
+      })
   }
 
   render() {
-    const { markdown } = this.state
+    const { activeSection, markdown } = this.state
 
     return (
       <Page stickHeader={true}>
         <Container>
           <Side>
             <Menu>
-              <Heading>Documentation</Heading>
+              <SidebarHeading>Documentation</SidebarHeading>
 
               {/* Search */}
               <SearchArea>
@@ -64,15 +109,25 @@ export default class Documentation extends Component {
               {/* Sections */}
               <Sections>
                 <SectionLinks>
-                  <SectionLink level={1} href={'#tutorial'}>
-                    Tutorial
-                  </SectionLink>
-                  <SectionLink level={1} href={'#collaboration'}>
-                    Collaboration issues in data science
-                  </SectionLink>
-                  <SectionLink level={1} href={'#tools'}>
-                    Tools for data scientists
-                  </SectionLink>
+                  {
+                    sidebar.map(({ name, links = [] }, index) => {
+                      const isActive = activeSection === index;
+                      return (
+                        <div key={index}>
+                          <SectionLink level={1} onClick={this.selectSection(index)} isActive={isActive}>
+                            {name}
+                          </SectionLink>
+                          <Collapse items={links.length} isOpen={isActive}>
+                            {links && links.map((link, idx) => (
+                              <SectionLink level={2} onClick={this.scrollToLink(link.href)} key={idx}>
+                                {link.name}
+                              </SectionLink>
+                            ))}
+                          </Collapse>
+                        </div>
+                      )
+                    })
+                  }
                 </SectionLinks>
               </Sections>
 
@@ -87,7 +142,8 @@ export default class Documentation extends Component {
               className="markdown-body"
               source={markdown}
               renderers={{
-                code: CodeBlock
+                code: CodeBlock,
+                heading: HeadingRenderer,
               }}
             />
           </Content>
@@ -137,7 +193,7 @@ const Menu = styled.div`
   `};
 `
 
-const Heading = styled.h3`
+const SidebarHeading = styled.h3`
   font-size: 24px;
   color: #b0b8c5;
 `
@@ -163,6 +219,8 @@ const Content = styled.article`
 
 const Sections = styled.div`
   margin-bottom: 40px;
+  margin-top: 10px;
+  min-width: 280px;
 `
 
 const SectionLinks = styled.div`
@@ -178,10 +236,11 @@ const SectionLink = styled.a`
   font-weight: 500;
   color: #b0b8c5;
   text-decoration: none;
-
+  font-weight: 400;
   line-height: 26px;
   min-height: 26px;
   margin-bottom: 5px;
+  cursor: pointer;
 
   &:hover {
     color: #3c3937;
@@ -208,15 +267,21 @@ const SectionLink = styled.a`
     props.level === 2 &&
     `
       padding-left: 44px;
-      
+
       &::before {
         display: none;
       }
   `};
 
   ${props =>
-    props.underlined &&
+    props.isActive &&
     `
-	  font-weight: bold;
+    color: #333;
 	`};
+`
+
+const Collapse = styled.div`
+  height: ${({ items, isOpen }) => isOpen ? items * 30 + 5*(!!items) : 0}px
+  overflow: hidden;
+  transition: height .3s ease;
 `
