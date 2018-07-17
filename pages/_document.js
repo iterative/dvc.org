0962e5f2b7e3440951cc951905752b886a09cb14
Andrey Shelkovnikov <shelkov09@gmail.com>
2018-07-17 23:38:53 +0300

    inject docsearch, split up head scripts to pages

--- pages/_document.js
+++ pages/_document.js
@@ -74,9 +74,6 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/static/favicon.ico" />
           <link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32" />
           <link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16" />
-          <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" />
           {this.props.styleTags}
         </Head>
         <body>
