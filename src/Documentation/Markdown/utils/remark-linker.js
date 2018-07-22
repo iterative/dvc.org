377937f03cec2c0e57deaa9fc44751232bc5845f
Ivan Shcheklein <shcheklein@gmail.com>
2018-07-22 16:21:24 -0700

    improve linker to include sub-command

--- src/Documentation/Markdown/utils/remark-linker.js
+++ src/Documentation/Markdown/utils/remark-linker.js
@@ -7,9 +7,16 @@ function linker() {
   function transformer(tree) {
     visit(tree, 'inlineCode', function(node, index, parent) {
       if (parent.type !== 'link' && /dvc [a-z-.]+/.test(node.value)) {
+        let parts = node.value.split(' ')
+        let url = 'https://dvc.org/doc/commands-reference/' + parts[1]
+
+        if (parts.length >= 2) {
+          url += ("#" + parts[2])
+        }
+
         parent.children[index] = {
           type: 'link',
-          url: 'https://dvc.org/doc/commands-reference/' + node.value.split(' ')[1],
+          url: url,
           children: [node],
           position: node.position
         };
