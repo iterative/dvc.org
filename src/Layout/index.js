896068521808dae5cad12a1d9d6545c9d4d300b5
Andrey Shelkovnikov <shelkov09@gmail.com>
2018-08-03 07:25:44 +0300

    sticky-panel

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,30 +1,60 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-
-import { initGA, logPageView } from '../utils/ga'
-
+// components
 import TopMenu from '../TopMenu'
 import Footer from '../Footer'
 import HamburgerMenu from '../HamburgerMenu'
+// utils
+import { initGA, logPageView } from '../utils/ga'
+import throttle from 'lodash.throttle'
+
+function googleAnalytics() {
+  if (!window.GA_INITIALIZED) {
+    initGA()
+    window.GA_INITIALIZED = true
+  }
+  logPageView()
+}
 
 export default class Layout extends Component {
-  componentDidMount() {
-    if (!window.GA_INITIALIZED) {
-      initGA()
-      window.GA_INITIALIZED = true
+  constructor() {
+    super()
+    this.state = {
+      scrolled: false
     }
-    logPageView()
+    this.handleScrollThrottled = throttle(this.handleScroll, 300)
+  }
+
+  componentDidMount() {
+    googleAnalytics()
+    this.bodybag = document.getElementById('bodybag');
+    this.bodybag.addEventListener('scroll', this.handleScrollThrottled)
+    this.handleScroll()
+  }
+
+  componentWillUnmount() {
+    this.bodybag.removeEventListener('scroll', this.handleScrollThrottled)
+  }
+
+  handleScroll = (e) => {
+    const scrollTop = e ? e.target.scrollTop : 0;
+    this.setState({
+      scrolled: scrollTop > 25
+    })
   }
 
   render() {
-    const { children, stickHeader = false } = this.props
+    const { children } = this.props
+    const { scrolled } = this.state
 
     return (
       <Wrapper>
-        <TopMenu />
+        <TopMenu scrolled={scrolled} />
         <HamburgerMenu />
-        {children}
-        <Footer />
+        <Bodybag id="bodybag" ref={ref => this.bodybag = ref}>
+          {children}
+          <Footer />
+        </Bodybag>
       </Wrapper>
     )
   }
@@ -32,4 +62,16 @@ export default class Layout extends Component {
 
 const Wrapper = styled.div`
   overflow: hidden;
+`
+
+const Bodybag = styled.div`
+  position: fixed;
+  top: 80px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  transition: top .2s linear;
 `
