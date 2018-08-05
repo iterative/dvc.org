92cd7485e1d420e8d74c3ddd1dbcad94a21e3735
Ruslan Kuprieiev <kupruser@gmail.com>
2018-08-05 05:31:11 +0300

    download: update to 0.16.0

--- src/DownloadButton/index.js
+++ src/DownloadButton/index.js
@@ -14,19 +14,19 @@ const LINE = `line`
 const links = {
   [OSX]: {
     title: 'Mac OS',
-    url: `https://github.com/iterative/dvc/releases/download/0.15.2/dvc-0.15.2.pkg`
+    url: `https://github.com/iterative/dvc/releases/download/0.16.0/dvc-0.16.0.pkg`
   },
   [WINDOWS]: {
     title: 'Windows',
-    url: `https://github.com/iterative/dvc/releases/download/0.15.2/dvc-0.15.2.exe`
+    url: `https://github.com/iterative/dvc/releases/download/0.16.0/dvc-0.16.0.exe`
   },
   [LINUX]: {
     title: 'Linux Deb',
-    url: `https://github.com/iterative/dvc/releases/download/0.15.2/dvc_0.15.2_amd64.deb`
+    url: `https://github.com/iterative/dvc/releases/download/0.16.0/dvc_0.16.0_amd64.deb`
   },
   [LINUX_RPM]: {
     title: 'Linux RPM',
-    url: `https://github.com/iterative/dvc/releases/download/0.15.2/dvc-0.15.2-1.x86_64.rpm`
+    url: `https://github.com/iterative/dvc/releases/download/0.16.0/dvc-0.16.0-1.x86_64.rpm`
   },
   [UNKNOWN]: {
     title: 'pip install dvc'
