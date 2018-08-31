5c1c7699268ec902982812b698fd2423586a971d
Ruslan Kuprieiev <kupruser@gmail.com>
2018-08-31 05:39:05 +0300

    download: update to 0.18.10

--- src/DownloadButton/index.js
+++ src/DownloadButton/index.js
@@ -14,19 +14,19 @@ const LINE = `line`
 const links = {
   [OSX]: {
     title: 'Mac OS',
-    url: `https://github.com/iterative/dvc/releases/download/0.18.9/dvc-0.18.9.pkg`
+    url: `https://github.com/iterative/dvc/releases/download/0.18.10/dvc-0.18.10.pkg`
   },
   [WINDOWS]: {
     title: 'Windows',
-    url: `https://github.com/iterative/dvc/releases/download/0.18.9/dvc-0.18.9.exe`
+    url: `https://github.com/iterative/dvc/releases/download/0.18.10/dvc-0.18.10.exe`
   },
   [LINUX]: {
     title: 'Linux Deb',
-    url: `https://github.com/iterative/dvc/releases/download/0.18.9/dvc_0.18.9_amd64.deb`
+    url: `https://github.com/iterative/dvc/releases/download/0.18.10/dvc_0.18.10_amd64.deb`
   },
   [LINUX_RPM]: {
     title: 'Linux RPM',
-    url: `https://github.com/iterative/dvc/releases/download/0.18.9/dvc-0.18.9-1.x86_64.rpm`
+    url: `https://github.com/iterative/dvc/releases/download/0.18.10/dvc-0.18.10-1.x86_64.rpm`
   },
   [UNKNOWN]: {
     title: 'pip install dvc'
