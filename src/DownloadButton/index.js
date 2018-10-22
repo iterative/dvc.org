2ed06d683db45fdc9c4893ef22837a246c04b250
Ruslan Kuprieiev <kupruser@gmail.com>
2018-10-22 20:35:33 +0300

    download: switch back to 0.19.13 for now

--- src/DownloadButton/index.js
+++ src/DownloadButton/index.js
@@ -4,7 +4,7 @@ import styled, { css } from 'styled-components'
 import isClient from '../utils/isClient'
 import { logEvent } from '../utils/ga'
 
-const VERSION = `0.19.14`
+const VERSION = `0.19.13`
 const OSX = `osx`
 const WINDOWS = `win`
 const LINUX = `linux`
