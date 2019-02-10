e9f6ddcdf34d24cccb313193701f4e9cb583e315
Ruslan Kuprieiev <kupruser@gmail.com>
2019-02-11 00:33:28 +0200

    download: update to 0.26.0

--- src/DownloadButton/index.js
+++ src/DownloadButton/index.js
@@ -4,7 +4,7 @@ import styled, { css } from 'styled-components'
 import isClient from '../utils/isClient'
 import { logEvent } from '../utils/ga'
 
-const VERSION = `0.25.4`
+const VERSION = `0.26.0`
 const OSX = `osx`
 const WINDOWS = `win`
 const LINUX = `linux`
