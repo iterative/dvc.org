b0637cbfa072acea12153795027df6e6ed4ff76d
Mr. Outis <mroutis@protonmail.com>
2019-02-12 09:43:06 -0600

    upgpkg: docsearch 2.6.2

--- pages/doc.js
+++ pages/doc.js
@@ -25,9 +25,9 @@ import sidebar from '../src/Documentation/sidebar'
 const HeadInjector = ({ sectionName = 'Documentation' }) => (
   <Head>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2.6.2/dist/cdn/docsearch.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/perfect-scrollbar@1.4.0/css/perfect-scrollbar.min.css" />
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js" />
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2.6.2/dist/cdn/docsearch.min.js" />
     <title>{sectionName} | Machine Learning Version Control System</title>
   </Head>
 );
