b11d5ad00faef5df8216880d4988d317135dd796
Ivan Shcheklein <shcheklein@gmail.com>
2019-03-30 20:57:34 -0700

    attempt to fix index title

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,6 @@ const HeadInjector = () => (
   <Head>
     <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-    <title>Machine Learning Version Control System</title>
   </Head>
 )
 
