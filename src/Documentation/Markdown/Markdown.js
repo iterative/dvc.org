f247b58f560662c5ca7d839634180498e84cc004
Kurian Benoy <kurian.bkk@gmail.com>
2019-05-14 06:59:57 +0530

    Change Padding size attribute for code blocks

--- src/Documentation/Markdown/Markdown.js
+++ src/Documentation/Markdown/Markdown.js
@@ -61,7 +61,8 @@ const HtmlRenderer = props => {
 const CodeBlock = ({ value, language }) => {
   const dvcStyle = Object.assign({}, docco)
   dvcStyle['hljs-comment'] = { color: '#999' }
-  dvcStyle['hljs-meta'] = { color: '#333', fontSize: '14px', paddingLeft: '8em' }
+  dvcStyle['hljs-meta'] = { color: '#333', fontSize: '14px' }
+  dvcStyle['hljs']['padding'] = '0.5em 0.5em 0.5em 2em'
   return (
     <SyntaxHighlighter language={language} style={dvcStyle}>
       {value}
