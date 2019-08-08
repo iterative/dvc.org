1833075ec89c9bb0ab408e24e947fc50b8b8fe81
Alexey Ivanov <mail@alexeyivanov.info>
2019-08-08 23:45:41 +0500

    Fix #392
    - Chech if page exists and only create link it if it exists
    - If subcommand page exists link to page, if not, create anchor

--- src/Documentation/Markdown/utils/remark-linker.js
+++ src/Documentation/Markdown/utils/remark-linker.js
@@ -1,23 +1,36 @@
 ;`use strict`
 
 import visit from 'unist-util-visit'
+import { getItemByPath } from '../../SidebarMenu/helper'
 
 function linker() {
   function transformer(tree) {
     visit(tree, 'inlineCode', function(node, index, parent) {
       if (parent.type !== 'link' && /dvc\s+[a-z-.]+/.test(node.value)) {
         let parts = node.value.split(/\s+/)
-        let url = '/doc/commands-reference/' + parts[1]
 
-        if (parts.length > 2) {
-          url += '#' + parts[2]
+        let tmpUrl = '/doc/commands-reference/'
+        let url
+
+        if (getItemByPath(tmpUrl + parts[1])) {
+          url = tmpUrl = tmpUrl + parts[1]
         }
 
-        parent.children[index] = {
-          type: 'link',
-          url: url,
-          children: [node],
-          position: node.position
+        if (parts.length > 2) {
+          if (getItemByPath(tmpUrl + parts[2])) {
+            url += '/' + parts[2]
+          } else {
+            url += '#' + parts[2]
+          }
+        }
+
+        if (url) {
+          parent.children[index] = {
+            type: 'link',
+            url: url,
+            children: [node],
+            position: node.position
+          }
         }
       }
     })
