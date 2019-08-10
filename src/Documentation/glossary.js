8781a71e8955a4409b17e0be54faa47e88756c11
Jorge Orpinel <jorge@orpinel.com>
2019-08-09 19:35:54 -0500

    glossary: use new format with ES6 template literals
    per https://github.com/iterative/dvc.org/pull/522#discussion_r312658429

--- src/Documentation/glossary.js
+++ src/Documentation/glossary.js
@@ -18,44 +18,47 @@ also a Git repository.
     {
       name: 'DVC Cache',
       match: ['DVC cache', 'cache', 'cache directory'],
-      desc:
-        'The DVC cache is a hidden storage (by default located in the ' +
-        '`.dvc/cache` directory) for files that are under DVC control, and ' +
-        'their different versions. For more details, please refer to this ' +
-        '[document](/doc/user-guide/dvc-files-and-directories' +
-        '#structure-of-cache-directory).'
+      desc: `
+The DVC cache is a hidden storage (by default located in the \`.dvc/cache\`
+directory) for files that are under DVC control, and their different versions.
+For more details, please refer to this
+[document](/doc/user-guide/dvc-files-and-directories#structure-of-cache-directory).
+      `
     },
     {
       name: 'Data Artifact',
       match: ['data artifact', 'data artifacts'],
-      desc:
-        'Any data file or directory, as well as intermediate or final ' +
-        'result (such as extracted features or a ML model file) that is ' +
-        'under DVC control. Refer to [Data and Model Files Versioning]' +
-        '(/doc/use-cases/data-and-model-files-versioning) for more details.'
+      desc: `
+Any data file or directory, as well as intermediate or final result (such as
+extracted features or a ML model file) that is under DVC control. Refer to
+[Data and Model Files Versioning](/doc/use-cases/data-and-model-files-versioning)
+for more details.
+      `
     },
     {
       name: 'Import Stage',
       match: ['import stage', 'import stages'],
-      desc:
-        'Stage (DVC-file) created with the `dvc import` or `dvc import-url` ' +
-        'commands. They represent files or directories from external sources.'
+      desc: `
+Stage (DVC-file) created with the \`dvc import\` or \`dvc import-url\`
+commands. They represent files or directories from external sources.
+      `
     },
     {
       name: 'Output',
       match: ['output', 'outputs'],
-      desc:
-        'A file or a directory that is under DVC control. See `dvc add`, ' +
-        '`dvc run`, `dvc import`, `dvc import-url` commands.'
+      desc: `
+A file or a directory that is under DVC control. See \`dvc add\` \`dvc run\`,
+\`dvc import\`, \`dvc import-url\` commands.
+      `
     },
     {
       name: 'External Dependency',
       match: ['external dependency', 'external dependencies'],
-      desc:
-        'A DVC-file dependency with origin in an external source, for ' +
-        'example HTTP, SSH, Amazon S3, Google Cloud Storage remote ' +
-        'locations, or even other DVC repositories. See ' +
-        '[External Dependencies](/doc/user-guide/external-dependencies).'
+      desc: `
+A DVC-file dependency with origin in an external source, for example HTTP, SSH,
+Amazon S3, Google Cloud Storage remote locations, or even other DVC repositories.
+See [External Dependencies](/doc/user-guide/external-dependencies).
+      `
     }
   ]
 }
