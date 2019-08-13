04aa7616d7eb4dd9c7cb99976f639de81b24cd7d
Dashamir Hoxha <dashohoxha@gmail.com>
2019-08-13 09:14:53 +0200

    Accept a list of files as arguments

--- scripts/prettier.sh
+++ scripts/prettier.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Run 'npx prettier' on all .js and .md files.
+# Run 'npx prettier' on the given .js or .md files. If no files are
+# given, run it on all the .js and .md files of the project.
 
 cd $(dirname $0)
 cd ..
 
 shopt -s globstar
-files=$(npx prettier --list-different src/**/*.js pages/*.js static/**/*.md)
-[[ -n $files ]] && npx prettier --write $files
+files="$@"
+[[ -z $files ]] && files='src/**/*.js pages/*.js static/**/*.md'
+fix_files=$(npx prettier --list-different $files)
+[[ -n $fix_files ]] && npx prettier --write $fix_files
 
 
