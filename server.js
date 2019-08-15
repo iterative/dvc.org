7c36dc9c040309d78c3c86126d83aa65798a84da
Jorge Orpinel <jorge@orpinel.com>
2019-08-14 23:42:00 -0500

    server: fix man.dvc.org/get-url redirect bug
    per https://github.com/iterative/dvc.org/issues/560#issuecomment-521511129

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // This file doesn't go through babel or webpack transformation.
 // Make sure the syntax and sources this file requires are compatible with the
-// current node version you are running
+// current node version you are running.
 // See https://github.com/zeit/next.js/issues/1245 for discussions on Universal
-// Webpack or universal Babel
+// Webpack or universal Babel.
+
 const { createServer } = require('http')
 const { parse } = require('url')
 const _ = require('force-ssl-heroku')
@@ -25,7 +26,9 @@ app.prepare().then(() => {
 
     if (req.headers.host === 'man.dvc.org') {
       let normalized_pathname =
-        pathname !== '/import-url' ? pathname.replace(/-/i, '/') : pathname
+        ['/get-url', '/import-url'].indexOf(pathname) > 0
+          ? pathname.replace(/-/i, '/')
+          : pathname
       const doc_pathname = '/doc/commands-reference' + normalized_pathname
       res.writeHead(301, { Location: 'https://dvc.org' + doc_pathname })
       res.end()
