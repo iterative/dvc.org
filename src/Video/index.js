fd4fe9c01c9d869aa960950bbaea0359b4f38ec4
Aman Sharma <mannu.poski10@gmail.com>
2019-10-21 03:14:30 +0530

    Wrap strings at 80 characters

--- src/Video/index.js
+++ src/Video/index.js
@@ -75,7 +75,10 @@ export default class Video extends Component {
             title="Video"
             width="560"
             height="315"
-            src={`https://www.youtube.com/embed/${id}?rel=0&amp;controls=0&amp;showinfo=0;${playing}`}
+            src={`
+              https://www.youtube.com/embed/${id}?
+              rel=0&amp;controls=0&amp;showinfo=0;${playing}
+            `}
             frameBorder="0"
             allow="autoplay; encrypted-media"
             allowFullScreen
