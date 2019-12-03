64b0749fd482d5cae655af68f63118642eef7403
Alexey Ivanov <mail@alexeyivanov.info>
2019-12-03 19:49:32 +0900

    Upadte consts

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,7 @@
 export const HEADER = 'header'
 
+const WEBSITE_HOST = 'dvc.org'
+
 export const META_BASE_TITLE = 'Data Version Control · DVC'
 export const META_DESCRIPTION =
   'Open-source version control system for Data Science and Machine Learning ' +
@@ -7,7 +9,7 @@ export const META_DESCRIPTION =
   'experiments.'
 export const META_KEYWORDS =
   'data version control machine learning models management'
-export const META_SOCIAL_IMAGE = 'https://dvc.org/static/social-share.png'
+export const META_SOCIAL_IMAGE = `https://${WEBSITE_HOST}/static/social-share.png`
 
-export const FORUM_URL = 'https://discuss.dvc.org'
-export const BLOG_URL = 'https://blog.dvc.org'
+export const FORUM_URL = `https://discuss.${WEBSITE_HOST}`
+export const BLOG_URL = `https://blog.${WEBSITE_HOST}`
