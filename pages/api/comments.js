c853e79359c6702cc38049718c5f1f71eecc3095
Jorge Orpinel <jorge@orpinel.com>
2019-12-05 11:34:04 -0800

    wrap /api/comments file doc paragraph per https://github.com/iterative/dvc.org/pull/819#pullrequestreview-327795707

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,12 +1,10 @@
 /*
- * This API endpoint is used by https://blog.dvc.org
- * to get comments count for the post, it gets
- * discuss.dvc.org topic url as a param and returns
- * comments count or error.
+ * This API endpoint is used by our blog to get comments count for the post, it
+ * gets discuss.dvc.org topic url as a param and returns comments count or
+ * error.
  *
- * It made this way to configure CORS, reduce user's payload
- * and to add potential ability to cache comments count
- * in the future.
+ * It made this way to configure CORS, reduce user's payload and to add
+ * potential ability to cache comments count in the future.
  */
 
 import Cors from 'micro-cors'
