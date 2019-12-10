3f1524bc372d9c59b167751a438afd1d70ff69ef
Jorge Orpinel <jorge@orpinel.com>
2019-12-09 17:37:36 -0800

    SEO: expand list of remotes in main landing page meta info per https://github.com/iterative/dvc.org/pull/846#pullrequestreview-329431223

--- src/Diagram/index.js
+++ src/Diagram/index.js
@@ -41,8 +41,9 @@ const ColumnOne = () => (
     <Description fullWidth>
       <p>
         Version control machine learning models, data sets and intermediate
-        files. DVC connects them with code, and uses cloud storage, SSH, NAS,
-        etc. to store file contents.
+        files. DVC connects them with code, and uses Amazon S3, Microsoft Azure
+        Blob Storage, Google Drive, Google Cloud Storage, Aliyun OSS, SSH/SFTP,
+        HDFS, HTTP, network-attached storage, or rsync to store file contents.
       </p>
       <p>
         Full code and data provenance help track the complete evolution of every
