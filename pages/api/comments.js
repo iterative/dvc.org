795e922603fe0f4047d00a03d21460d2e0f863e0
Alexey Ivanov <mail@alexeyivanov.info>
2019-12-23 21:54:03 +0900

    Rewrote comments API using fetch

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -7,8 +7,8 @@
  * potential ability to cache comments count in the future.
  */
 
+import fetch from 'isomorphic-fetch'
 import Cors from 'micro-cors'
-import request from 'request'
 import NodeCache from 'node-cache'
 
 const cache = new NodeCache({ stdTTL: 900 })
@@ -20,7 +20,7 @@ const cors = Cors({
   origin: BLOG_URL
 })
 
-const getCommentCount = (req, res) => {
+const getCommentCount = async (req, res) => {
   const {
     query: { url }
   } = req
@@ -31,36 +31,40 @@ const getCommentCount = (req, res) => {
     return
   }
 
-  if (cache.get(url)) {
+  if (cache.get(url) !== undefined) {
     console.log(`Using cache for ${url}`)
     res.status(200).json({ count: cache.get(url) })
 
     return
   }
 
-  request(`${url}.json`, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
+  try {
+    const response = await fetch(`${url}.json`)
+
+    if (response.status !== 200) {
       res.status(502).json({ error: 'Unexpected response from Forum' })
 
       return
     }
 
-    const json = JSON.parse(body)
+    const data = await response.json()
 
-    if (!json.posts_count) {
+    if (!data.posts_count) {
       res.status(502).json({ error: 'Unexpected payload from Forum' })
 
       return
     }
 
     // post_count return all posts including topic itself
-    const count = json.posts_count - 1
+    const count = data.posts_count - 1
 
     console.log(`Not using cache for ${url}`)
     cache.set(url, count)
 
     res.status(200).json({ count })
-  })
+  } catch {
+    res.status(404)
+  }
 }
 
 export default cors(getCommentCount)
