d2293b58de266938ad5732a7786a6736a8f5ce39
Jorge Orpinel <jorge@orpinel.com>
2020-01-10 11:56:57 -0600

    user-guide: add HTTP redirect from err.dvc.org/{header} to troubleshooting guide header per https://github.com/iterative/dvc.org/pull/875#issuecomment-573035112

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.prepare().then(() => {
             : pathname)
       })
       res.end()
+    } else if (req.headers.host === 'err.dvc.org') {
+      // err.dvc.org/{hdr} -> dvc.org/doc/user-guide/troubleshooting#{hdr},
+      res.writeHead(301, {
+        'Cache-Control': 'no-cache',
+        Location: `https://dvc.org/doc/user-guide/troubleshooting#${pathname}`
+      })
+      res.end()
     } else if (/^(code|data|remote)\.dvc\.org$/.test(req.headers.host)) {
       // {code/data/remote}.dvc.org -> corresponding S3 bucket
       res.writeHead(301, {
