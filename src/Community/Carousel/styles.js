4186cc7e4f1f2da51d5f1391840d9ef91506ac17
Alexey Ivanov <mail@alexeyivanov.info>
2020-01-29 00:55:48 +0900

    Remove focus state from carousel dots

--- src/Community/Carousel/styles.js
+++ src/Community/Carousel/styles.js
@@ -2,4 +2,8 @@ import styled from 'styled-components'
 
 export const SliderWrapper = styled.div`
   margin-bottom: 10px;
+
+  .slick-dots li:not(.slick-active) button:focus:before {
+    opacity: 0.25;
+  }
 `
