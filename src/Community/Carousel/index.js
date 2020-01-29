b1b3f8d4f223bdf7a6cdd7841c567332474a142b
Alexey Ivanov <mail@alexeyivanov.info>
2020-01-29 10:43:46 +0900

    Temporarily disable carousel

--- src/Community/Carousel/index.js
+++ src/Community/Carousel/index.js
@@ -1,36 +1,3 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Slider from 'react-slick'
-
-import { OnlyDesktop, OnlyMobile } from '../../styles'
-
-import { SliderWrapper } from './styles'
-
-const imagesSliderProps = {
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  initialSlide: 1,
-  adaptiveHeight: true,
-  infinite: true,
-  autoplay: true,
-  speed: 600,
-  dots: true,
-  arrows: false
-}
-
 export default function CommunityCarousel({ items }) {
-  return (
-    <>
-      <OnlyDesktop>{items}</OnlyDesktop>
-      <OnlyMobile>
-        <SliderWrapper>
-          <Slider {...imagesSliderProps}>{items}</Slider>
-        </SliderWrapper>
-      </OnlyMobile>
-    </>
-  )
-}
-
-CommunityCarousel.propTypes = {
-  items: PropTypes.array
+  return items
 }
