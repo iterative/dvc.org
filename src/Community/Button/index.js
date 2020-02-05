f45e1bf6dc8f21436f5d52d0c86837fee2704f8f
Alexey Ivanov <mail@alexeyivanov.info>
2020-02-05 23:07:14 +0900

    Add ref forwarding to button

--- src/Community/Button/index.js
+++ src/Community/Button/index.js
@@ -3,18 +3,24 @@ import PropTypes from 'prop-types'
 
 import { Wrapper } from './styles'
 
-export default function Button({ theme, children, ...props }) {
+function BareButton({ theme, children, forwardedRef, ...props }) {
   return (
-    <Wrapper {...props} {...theme}>
+    <Wrapper {...props} {...theme} ref={forwardedRef}>
       {children}
     </Wrapper>
   )
 }
 
-Button.propTypes = {
+BareButton.propTypes = {
   children: PropTypes.node,
   theme: PropTypes.shape({
     backgroundColor: PropTypes.string,
     color: PropTypes.string
-  })
+  }),
+  forwardedRef: PropTypes.func
 }
+
+// eslint-disable-next-line react/display-name
+export default React.forwardRef((props, ref) => (
+  <BareButton {...props} forwardedRef={ref} />
+))
