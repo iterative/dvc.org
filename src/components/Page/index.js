f7845fb8873e73949334ff8d77ccd82398bde7a1
Pavel Grinchenko <psdcoder@gmail.com>
2020-03-16 14:51:10 +0300

    Add handler for anchor navigation

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,23 +1,38 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from '@reach/router'
 import PropTypes from 'prop-types'
 
 import Layout from '../Layout'
 
 import { GlobalStyle } from '../../styles'
 
-export default class Page extends Component {
-  render() {
-    const { children, ...rest } = this.props
+const useAnchorNavigation = () => {
+  const location = useLocation()
 
-    return (
-      <>
-        <GlobalStyle />
-        <Layout {...rest}>{children}</Layout>
-      </>
-    )
-  }
+  useEffect(() => {
+    if (location.hash) {
+      const node = document.querySelector(location.hash)
+
+      if (node) {
+        node.scrollIntoView()
+      }
+    }
+  }, [location.href])
+}
+
+const Page = ({ children, ...rest }) => {
+  useAnchorNavigation()
+
+  return (
+    <>
+      <GlobalStyle />
+      <Layout {...rest}>{children}</Layout>
+    </>
+  )
 }
 
 Page.propTypes = {
   children: PropTypes.node.isRequired
 }
+
+export default Page
