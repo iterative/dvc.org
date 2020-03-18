4871b26aa0cddd0d75c57fa800ab0e1ee1ab2567
Casper da Costa-Luis <casper.dcl@physics.org>
2020-03-18 00:46:31 +0000

    links: checks: support spaces in paths

--- scripts/link-check-git-diff.sh
+++ scripts/link-check-git-diff.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 differ="git diff $(git merge-base HEAD origin/master)"
-# caveat: can't accept spaces in paths
-for f in $($differ --name-only) ; do
+$differ --name-only | while read -r f ; do
   echo -n "$f:"
   $(dirname "$0")/link-check.sh <($differ -U0 -- "$f" | grep '^\+')
 done
