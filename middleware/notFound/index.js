8344ef6f10158d94fbcbf4484122f862caedb46d
Pavel Grinchenko <psdcoder@gmail.com>
2020-03-18 15:31:19 +0300

    Use 'serve-handler' package for handling trailings slashed in the proper way instead of express.static()

--- middleware/notFound/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-
-const path = require('path')
-
-module.exports = (req, res) => {
-  res.status(404)
-  res.sendFile(path.join(`${__dirname}`, '..', '..', 'public', '404.html'))
-}
