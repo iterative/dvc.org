411f4b0bbf6f0bcef04bb145b947300bbfd0d889
Casper da Costa-Luis <casper.dcl@physics.org>
2020-03-20 01:14:21 +0000

    fix again

--- scripts/link-check-git-diff.sh
+++ scripts/link-check-git-diff.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 exclude="${CHECK_LINKS_EXCLUDE_LIST:-$(dirname $0)/exclude-links.txt}"
 differ="git diff $(git merge-base HEAD origin/master)"
-changed="$($differ --name-only | grep -v "$(basename "$exclude")")"
+changed="$($differ --name-only)"
 
 [ -z "$changed" ] && exit 0
 
-echo "$changed" | while read -r file ; do
+echo "$changed" | grep -v "$(basename "$exclude")" | while read -r file ; do
   echo -n "$file:"
   $(dirname "$0")/link-check.sh <($differ -U0 -- "$file" | grep '^\+')
 done
