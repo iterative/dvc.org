566a5410083afbcfbd16ef8e4fa2b195e30e62ad
Casper da Costa-Luis <casper.dcl@physics.org>
2020-03-20 00:24:53 +0000

    tests: links: exclude exclusions on diff

--- scripts/link-check-git-diff.sh
+++ scripts/link-check-git-diff.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+exclude="${CHECK_LINKS_EXCLUDE_LIST:-$(dirname $0)/exclude-links.txt}"
 differ="git diff $(git merge-base HEAD origin/master)"
-$differ --name-only | while read -r file ; do
+
+$differ --name-only | grep -v "$(basename "$exclude")" | while read -r file ; do
   echo -n "$file:"
   $(dirname "$0")/link-check.sh <($differ -U0 -- "$file" | grep '^\+')
 done
