0097c68069f29fc8f13a2778c3d9d459b5a4fe74
Pavel Grinchenko <psdcoder@gmail.com>
2020-03-22 12:12:40 +0300

    Specify Sentry config to ignore ChunkLoadError and errors from browsers extensions

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,22 @@ const plugins = [
     options: {
       dsn: process.env.SENTRY_DSN,
       environment: process.env.NODE_ENV,
-      enabled: process.env.NODE_ENV === 'production'
+      release: process.env.CIRCLE_SHA1,
+      enabled: process.env.NODE_ENV === 'production',
+      ignoreErrors: [
+        /* When we deploy new version we delete assets which were generated for
+        the previous deployed version, but users can have opened old version in 
+        their browsers. If they hover some link on the page Gatsby.js will try
+        fetch old chunks and will get ChunkLoadError, but then will load static
+        page from the new deployed version and all will be ok. So we can just
+        ignore these type of errors */
+        'ChunkLoadError'
+      ],
+      /* There are some common urls which recomment to ignore. It's even 
+      mentioned in the official documentation: https://docs.sentry.io/platforms/javascript/#decluttering-sentry
+      In our case we just ignore all errors from the browser's extensions,
+      because we can't influence on then somehow. */
+      blacklistUrls: [/extensions\//i, /^chrome:\/\//i]
     }
   }
 ]
