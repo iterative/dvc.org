7ec89453c768f3e9f036c1f21ebeef21d2491c08
Pavel Grinchenko <psdcoder@gmail.com>
2020-03-30 19:19:58 +0300

    Transform initial pages components to TS

---
+++ src/pages/community.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Community from '../components/Community'
+import SEO from '../components/SEO'
+
+const CommunityPage: React.SFC = () => (
+  <>
+    <SEO title="Community" />
+    <Community />
+  </>
+)
+
+export default CommunityPage
---
+++ src/pages/support.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import SEO from '../components/SEO'
+import Support from '../components/Support'
+
+const SupportPage: React.SFC = () => (
+  <>
+    <SEO title="Support" />
+    <Support />
+  </>
+)
+
+export default SupportPage
