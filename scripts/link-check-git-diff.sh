2920b27c21b3dfcf59299bce216c4307fff34161
Casper da Costa-Luis <casper.dcl@physics.org>
2020-04-19 11:44:41 +0100

    test: fix link-check diffs
    Fixes #1148

--- scripts/link-check-git-diff.sh
+++ scripts/link-check-git-diff.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 exclude="${CHECK_LINKS_EXCLUDE_LIST:-$(dirname $0)/exclude-links.txt}"
 differ="git diff $(git merge-base HEAD origin/master)"
-changed="$differ --name-only :^redirects-list.json"
+changed="$($differ --name-only)"
 
 [ -z "$changed" ] && exit 0
 
