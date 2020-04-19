4dad284a3553cf43a9da2549fadd80730f8b4b4c
Casper da Costa-Luis <casper.dcl@physics.org>
2020-04-19 15:01:48 +0100

    test: link-check: misc tidy

--- scripts/link-check-git-all.sh
+++ scripts/link-check-git-all.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 repo="$(dirname "$(realpath "$(dirname "$0")")")"
+pushd "$repo"
 
 # can't do git ls-files since some may be untracked
-(find "$repo"/.github/ "$repo"/content/docs/ "$repo"/src/ -name '*.md' -o -name '*.js' && ls "$repo"/*.md "$repo"/*.js) \
-  | xargs -n1 -P8 $(dirname "$0")/link-check.sh
+(find .github/ content/docs/ src/ -name '*.md' -o -name '*.js' && ls *.md *.js) \
+  | xargs -n1 -P8 "$(dirname "$0")"/link-check.sh
+
+popd
