8af9c72ef821f9614abfc38e2021c8ef5cf6fcda
Casper da Costa-Luis <casper.dcl@physics.org>
2020-04-19 12:29:35 +0100

    test: link-check: use git pathspec exclusions

--- scripts/link-check-git-diff.sh
+++ scripts/link-check-git-diff.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 exclude="${CHECK_LINKS_EXCLUDE_LIST:-$(dirname $0)/exclude-links.txt}"
 differ="git diff $(git merge-base HEAD origin/master)"
-changed="$($differ --name-only)"
+changed="$($differ --name-only -- . ':!'"$exclude" ':!redirects-list.json')"
 
 [ -z "$changed" ] && exit 0
 
-echo "$changed" | grep -v "$(basename "$exclude")" | while read -r file ; do
+echo "$changed" | while read -r file ; do
   # check whole file
   # "$(dirname "$0")"/link-check.sh "$file"
   # check just changed lines
