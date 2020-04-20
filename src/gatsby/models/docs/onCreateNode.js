4a3cdc4059009d4c1db2f9b8d918abd852d6e65a
RMP <rogermparent@gmail.com>
2020-04-20 15:06:33 -0400

    Clean up docs/onCreateNode
    - Move the "special exemption" comment to where it's more appropriate.
    - Rename pagePath to slug to keep with this project's conventions.

--- src/gatsby/models/docs/onCreateNode.js
+++ src/gatsby/models/docs/onCreateNode.js
@@ -3,20 +3,20 @@ const path = require('path')
 function createMarkdownDocsNode(api, options, { parentNode }) {
   const splitDir = parentNode.relativeDirectory.split('/')
   if (splitDir[0] !== 'docs') return
-  // Make a special exemption for the root doc.
 
   const { node, actions, createNodeId, createContentDigest } = api
   const { createNode, createParentChildLink } = actions
   const { name, relativePath } = parentNode
   splitDir[0] = 'doc'
 
-  const pagePath =
+  // Make a special exemption for the root doc.
+  const slug =
     parentNode.relativePath === 'docs/index.md'
       ? '/doc/home'
       : path.posix.join('/', ...splitDir, name === 'index' ? '/' : name)
 
   const fieldData = {
-    slug: pagePath,
+    slug,
     rawMarkdownBody: node.rawMarkdownBody,
     sourcePath: relativePath
   }
