dbfcdab124b238ac0c3ef5a137c765d452ddeefb
RMP <rogermparent@gmail.com>
2020-04-20 16:13:32 -0400

    Change the Model API builder into a simple reusable wrapper.
    This is before places that use the builder are updated, so this commit doesn't work.

--- src/gatsby/utils/models/index.js
+++ src/gatsby/utils/models/index.js
@@ -1,28 +1,31 @@
-/** Model API builder
-    (models:Array<Model>, childApi:Any?) => (name:String) => (api, options) => Promise
+/** Model API runner
+  (
+    models: Array<Model>
+      The array of models to run the action on.
+    name: string
+      If any model has a function under under this key, that function will be run.
+    ...childArgs
+      All arguments passed after models and name are forwarded to the child calls.
+      Generally, you'd put the Gatsby API object here and then any other data to
+      pass along.
+  )
 
-    General usage:
+  Standard usage:
+    In a Gatsby API, this mimics the original Gatsby call on all Models:
 
-    1. In gatsby-node, outside of any api calls, store the result of this function called with
-       an array of all models.
-    2. Call the function returned from step 1 with the name of the function you want to run on
-       all models.
-    3. Use the value from step 2 as a gatsby call, or manually call it within another function using
-       the first two positional parameters from Gatsby.
-    4. Return or await the promise received from step 3. Don't worry about the promises' return values.
- */
-function buildModelApi(models, childAPI = { models }) {
-  return function apiRunnerFromName(name) {
-    return async function runApiWithGatsby(api, options) {
-      return Promise.all(
-        models.map(
-          model =>
-            typeof model[name] === 'function' &&
-            model[name](api, options, childAPI)
-        )
-      )
-    }
-  }
+    exports.onCreateNode = (api) =>
+      asyncCallOnAll(models, "onCreateNode", api)
+
+    You can also call this within another function if you want to use Models
+    within a regular gatsby-node function.
+*/
+
+async function asyncCallOnAll(models, apiName, ...childArgs) {
+  return Promise.all(
+    models.map(
+      model => typeof model[name] === 'function' && model[name](...childArgs)
+    )
+  )
 }
 
-module.exports = buildModelApi
+module.exports = asyncCallOnAll
