2ba1f2cb3dafb502da1fd8ae4e734f455e2fc46c
RMP <rogermparent@gmail.com>
2020-04-20 20:56:55 -0400

    Make blog page limiter work on an env variable
    - Initialize dotenv in `gatsby-node` to ensure it's loaded for all models.
    
    - Make the Blog Page limiter in the blog/createPages module trigger only
      when LIMIT_BLOG_PAGES is truthy.

--- src/gatsby/models/blog/createPages.js
+++ src/gatsby/models/blog/createPages.js
@@ -1,6 +1,12 @@
 const path = require('path')
 const tagToSlug = require('../../../utils/shared/tagToSlug')
 const { BLOG } = require('../../../consts')
+// Since blog pages and their indexes require a ton of image resizes, it's
+// useful to have an option to only generate a minimal set of these pages when
+// developing. Set LIMIT_BLOG_PAGES to anything truthy and this module will
+// attempt to generate as few blog pages as possible while still having a bit of
+// everything to look at.
+const LIMIT_BLOG_PAGES = Boolean(process.env.LIMIT_BLOG_PAGES)
 
 const pageUrl = (basePath, page) => {
   if (page > 1) {
@@ -47,15 +53,10 @@ function* pagesGenerator({ itemCount, hasHeroItem = false, basePath }) {
 }
 
 const createPages = async ({ graphql, actions }) => {
-  const isDevMode = process.env.NODE_ENV === 'development'
-
   const blogResponse = await graphql(
     `
-      {
-        allBlogPost(sort: { fields: [date], order: DESC }, limit: 9999) {
-          pageInfo {
-            itemCount
-          }
+      query BlogPageBuilderQuery($limit: Int) {
+        allBlogPost(sort: { fields: [date], order: DESC }, limit: $limit) {
           tags: group(field: tags) {
             fieldValue
             pageInfo {
@@ -68,18 +69,17 @@ const createPages = async ({ graphql, actions }) => {
           }
         }
       }
-    `
+    `,
+    {
+      limit: LIMIT_BLOG_PAGES ? 1 : 9999
+    }
   )
 
   if (blogResponse.errors) {
     throw blogResponse.errors
   }
 
-  const {
-    pageInfo: { itemCount },
-    tags,
-    nodes: posts
-  } = blogResponse.data.allBlogPost
+  const { tags, nodes: posts } = blogResponse.data.allBlogPost
 
   // Create home blog pages (with pagination)
   const blogHomeTemplate = path.resolve('./src/templates/blog-home.tsx')
@@ -87,7 +87,7 @@ const createPages = async ({ graphql, actions }) => {
   for (const page of pagesGenerator({
     basePath: '/blog',
     hasHeroItem: true,
-    itemCount: isDevMode ? 1 : itemCount
+    itemCount: posts.length
   })) {
     actions.createPage({
       component: blogHomeTemplate,
@@ -102,10 +102,8 @@ const createPages = async ({ graphql, actions }) => {
   // Create blog posts pages
   const blogPostTemplate = path.resolve('./src/templates/blog-post.tsx')
 
-  const _posts = isDevMode ? posts.slice(0, 1) : posts
-
   const blogPagesPromise = Promise.all(
-    _posts.map(({ id, slug }, index) => {
+    posts.map(({ id, slug }, index) => {
       const previous = index === posts.length - 1 ? null : posts[index + 1]
       const next = index === 0 ? null : posts[index - 1]
 
@@ -126,7 +124,11 @@ const createPages = async ({ graphql, actions }) => {
   // Create tags pages (with pagination)
   const blogTagsTemplate = path.resolve('./src/templates/blog-tags.tsx')
 
-  const _tags = isDevMode ? tags.slice(0, 1) : tags
+  // We have to explicitly limit tag pages here, otherwise we get one for
+  // every tag on the example post that makes images for a few children.
+  // That can easily add hundreds of images because of the blog index template.
+  const _tags = LIMIT_BLOG_PAGES ? tags.slice(0, 1) : tags
+
   const tagPagesPromise = Promise.all(
     _tags.map(({ fieldValue: tag, pageInfo: { itemCount } }) => {
       const basePath = `/tags/${tagToSlug(tag)}`
