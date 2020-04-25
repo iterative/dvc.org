12ede4ecb7daaa9673a02ed1bb27558e3862a459
Casper da Costa-Luis <casper.dcl@physics.org>
2020-04-26 00:33:12 +0100

    link-check: add *.css

--- scripts/link-check-git-all.sh
+++ scripts/link-check-git-all.sh
@@ -6,7 +6,7 @@ pushd "$repo"
 
 # can't do git ls-files since some may be untracked
 (find .github/ content/docs/ src/ \
-  -name '*.js' -o -name '*.jsx' -o -name '*.md' -o -name '*.tsx' -o \
+  -name '*.css' -o -name '*.js' -o -name '*.jsx' -o -name '*.md' -o -name '*.tsx' -o \
   -name '*.ts' -o -name '*.json' && ls *.js *.jsx *.md *.tsx *.ts *.json) \
   | xargs -n1 -P8 "$(dirname "$0")"/link-check.sh
 
