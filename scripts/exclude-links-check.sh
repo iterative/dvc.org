0b709c6570054b4f5a2ac4e5c7221359a6df908b
Casper da Costa-Luis <casper.dcl@physics.org>
2020-04-26 20:25:42 +0100

    link-check: fix exclusions (#1190)
    * link-check: fix exclusions
    
    * link-check: add a couple of excludes

--- scripts/exclude-links-check.sh
+++ scripts/exclude-links-check.sh
@@ -3,11 +3,12 @@
 set -euo pipefail
 
 source "$(dirname "$0")"/utils.sh
+base_url="${CHECK_LINKS_RELATIVE_URL:-https://dvc.org}"
 exclude="${CHECK_LINKS_EXCLUDE_LIST:-$(dirname "$0")/exclude-links.txt}"
 [ -f "$exclude" ] && exclude="$(cat "$exclude")"
 
 missing="$(
-  urlfinder $(git ls-files '*.css' '*.js' '*.jsx' '*.md' '*.tsx' '*.ts' '*.json' ':!redirects-list.json' ':!*.test.js') \
+  urlfinder "$base_url" $(git ls-files '*.css' '*.js' '*.jsx' '*.md' '*.tsx' '*.ts' '*.json' ':!redirects-list.json' ':!*.test.js') \
   | sed 's/#.*//g' | sort -u \
   | comm -13 - <(echo "$exclude" | sort -u)
 )"
--- scripts/link-check-git-all.sh
+++ scripts/link-check-git-all.sh
@@ -7,8 +7,8 @@ pushd "$repo"
 # can't do git ls-files since some may be untracked
 (find .github/ content/docs/ src/ \
   -name '*.css' -o -name '*.js' -o -name '*.jsx' -o -name '*.md' -o -name '*.tsx' -o \
-  -name '*.ts' -o -name '*.json' && ls *.js *.jsx *.md *.tsx *.ts *.json) \
-  | grep -Ev '(redirects-list\.json|\.test\.js)$' \
+  -name '*.ts' -o -name '*.json' && ls *.css *.js *.jsx *.md *.tsx *.ts *.json) \
+  | grep -Ev '(package-lock\.json|redirects-list\.json|\.test\.js)$' \
   | xargs -n1 -P8 "$(dirname "$0")"/link-check.sh
 
 popd
