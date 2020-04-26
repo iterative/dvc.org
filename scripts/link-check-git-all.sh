5bb52cdda8757d9e62a9c258a23deee9567bbd77
Casper da Costa-Luis <casper.dcl@physics.org>
2020-04-26 16:55:58 +0100

    link-check: exclude *.test.js
    also ensure link-check-git-all follows exclusions

--- scripts/link-check-git-all.sh
+++ scripts/link-check-git-all.sh
@@ -8,6 +8,7 @@ pushd "$repo"
 (find .github/ content/docs/ src/ \
   -name '*.css' -o -name '*.js' -o -name '*.jsx' -o -name '*.md' -o -name '*.tsx' -o \
   -name '*.ts' -o -name '*.json' && ls *.js *.jsx *.md *.tsx *.ts *.json) \
+  | grep -Ev '(redirects-list\.json|\.test\.js)$' \
   | xargs -n1 -P8 "$(dirname "$0")"/link-check.sh
 
 popd
