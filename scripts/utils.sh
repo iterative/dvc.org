e4ec88d7e217d9fbc26d307cd621487db0f3dea3
Casper da Costa-Luis <casper.dcl@physics.org>
2020-05-04 22:52:53 +0100

    link-check: ignore URLs preceded by (#1234)

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -13,7 +13,7 @@ urlfinder(){  # expects <base_url> <files>...
   base_url="$1"
   content="$(cat "${@:2}")"  # read once (could be file descriptors)
   # explicit links not in markdown
-  echo "$content" | pcregrep -o '(?<!\]\()https?://[^\s<>{}"'"'"'`]+'
+  echo "$content" | pcregrep -o '(?<!\]\(|#)https?://[^\s<>{}"'"'"'`]+'
   # explicit links in markdown
   echo "$content" | pcregrep -o '(?<=\])\(https?://[^[\]\s]+\)' | pcregrep -o '\((?:[^)(]*(?R)?)*+\)' | pcregrep -o '(?<=\().*(?=\))'
   # relative links in markdown
