7d19cc9f1517c7202fec45bbe1dc8bc052376cc1
rogermparent <rogermparent@gmail.com>
2020-05-30 19:15:28 -0400

    Remake string link processors
    The string processors now have their own file with an object that links hostname
    strings to processor functions that take a specific set of URL info gathered via
    regex and return an object describing Author link represented by the string.
    
    Now they're made of re-usable functional blocks, allowing for quick additions to
    recognize other site types. This allows us to easily re-use common patterns like
    "the username is the pathname with slashes stripped" while also being flexible
    enough to accomodate any special case, as there will always be exceptions.
    
    There's also tests for the new link parser.

--- src/gatsby/models/authors/onCreateMarkdownContentNode.js
+++ src/gatsby/models/authors/onCreateMarkdownContentNode.js
@@ -1,36 +1,4 @@
-function parseLink(input) {
-  if (typeof input === 'string') {
-    const url = new URL(input)
-    // Slice the 'www.' off of given hostnames to normalize them.
-    const hostname = url.hostname.startsWith('www.')
-      ? url.hostname.slice(4)
-      : url.hostname
-
-    switch (hostname) {
-      case 'twitter.com':
-        return {
-          site: 'twitter',
-          // Remove leading slash
-          username: url.pathname.slice(1),
-          url: input
-        }
-      case 'linkedin.com':
-        return {
-          site: 'linkedin',
-          // Remove '/in/'
-          username: url.pathname.slice(4),
-          url: input
-        }
-      default:
-        return {
-          site: null,
-          url: input
-        }
-    }
-  } else {
-    return input
-  }
-}
+const parseLink = require('./parse-link.js')
 
 async function createMarkdownAuthorNode(api, { parentNode, createChildNode }) {
   if (parentNode.relativeDirectory.split('/')[0] !== 'authors') return
