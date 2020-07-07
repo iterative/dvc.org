fe874a0eaec6e421aa8b555d5f90946e20e0f2d7
Ivan Shcheklein <shcheklein@gmail.com>
2020-07-07 14:06:24 -0700

    Update index.tsx

--- src/components/LayoutHeader/index.tsx
+++ src/components/LayoutHeader/index.tsx
@@ -29,7 +29,8 @@ const LayoutHeader: React.FC<Required<ILayoutModifiable>> = ({ modifiers }) => {
       />
       <div className={styles.header}>
         <div className={cn(styles.alert, collapsed && styles.collapsed)}>
-          🚀 Check out the newest tool, {' '}
+          <span role="img" aria-label="rocket">🚀</span>
+          Check out the newest tool, {' '}
           <Link href="https://cml.dev">CML</Link>!
         </div>
         <LayoutWidthContainer
