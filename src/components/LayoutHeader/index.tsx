9942ff8e429d72720f9ab84b1c83567e7a3b1377
Roger <rogermparent@gmail.com>
2020-07-07 23:35:51 -0400

    Add GitHub link icon to CML alert bar (#1546)
    * Add an alert box above the header to advertise CML
    
    Not the most reusable implementation, but it does the job.
    
    * Update index.tsx
    
    * Update styles.module.css
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Fix Alert+Nav issue
    
    Since the nav menu isn't in the header's tree, this fix has the menu icon invoke
    the header hook separately and restyle itself alongside the header.
    
    * Add a GitHub icon link to the CML alert bar
    
    This also fixes the SocialIcon GitHub link to be currentColor, as this commit
    re-uses it.
    
    Co-authored-by: Ivan Shcheklein <shcheklein@gmail.com>

--- src/components/LayoutHeader/index.tsx
+++ src/components/LayoutHeader/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import includes from 'lodash/includes'
 
 import { LayoutModifiers, ILayoutModifiable } from '../MainLayout'
+import { ReactComponent as GitHubIcon } from '../SocialIcon/github.svg'
 import LayoutWidthContainer from '../LayoutWidthContainer'
 import Link from '../Link'
 import Nav from './Nav'
@@ -32,7 +33,14 @@ const LayoutHeader: React.FC<Required<ILayoutModifiable>> = ({ modifiers }) => {
           <span role="img" aria-label="rocket">
             🚀
           </span>{' '}
-          Check out our newest tool, <Link href="https://cml.dev">CML</Link>!
+          Check out our newest tool, <Link href="https://cml.dev">CML</Link>!{' '}
+          <Link
+            className={styles.gitHubAlertLink}
+            href="https://github.com/iterative/cml"
+            title="Star us on GitHub!"
+          >
+            <GitHubIcon width="1em" height="1em" viewBox="5 5 30 30" />
+          </Link>
         </div>
         <LayoutWidthContainer
           className={cn(styles.container, collapsed && styles.collapsed)}
