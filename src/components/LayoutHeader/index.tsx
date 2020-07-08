8699a52b2d1c8ca2dc45f07faf14983b6efd2d0c
Roger <rogermparent@gmail.com>
2020-07-07 22:47:14 -0400

    CML alert bar extra fixes (#1545)
    * Add an alert box above the header to advertise CML
    
    Not the most reusable implementation, but it does the job.
    
    * Update index.tsx
    
    * Update styles.module.css
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Fix Alert+Nav issue
    
    Since the nav menu isn't in the header's tree, this fix has the menu icon invoke
    the header hook separately and restyle itself alongside the header.
    
    Co-authored-by: Ivan Shcheklein <shcheklein@gmail.com>

--- src/components/LayoutHeader/index.tsx
+++ src/components/LayoutHeader/index.tsx
@@ -31,9 +31,8 @@ const LayoutHeader: React.FC<Required<ILayoutModifiable>> = ({ modifiers }) => {
         <div className={cn(styles.alert, collapsed && styles.collapsed)}>
           <span role="img" aria-label="rocket">
             🚀
-          </span>
-          Check out the newest tool,
-          <Link href="https://cml.dev">CML</Link>!
+          </span>{' '}
+          Check out our newest tool, <Link href="https://cml.dev">CML</Link>!
         </div>
         <LayoutWidthContainer
           className={cn(styles.container, collapsed && styles.collapsed)}
