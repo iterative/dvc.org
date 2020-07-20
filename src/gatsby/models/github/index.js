4c9f55ef06784a0b8e127d54491abdd87040d2e8
Ivan Shcheklein <shcheklein@gmail.com>
2020-07-19 17:10:49 -0700

    Revert "Improve CloudFlare cache clear logging and GitHub Stars bugfix (#1587)"
    This reverts commit a7f4d15ef5ca03818ca0452a2f600232dfdf7f20.

--- src/gatsby/models/github/index.js
+++ src/gatsby/models/github/index.js
@@ -5,7 +5,7 @@
    gatsby-plugin-github-api. If no GITHUB_TOKEN is specified, this node will be
    filled with default dummy data instead of throwing like using the plugin
    alone does.
- */
+*/
 
 async function createStaticGithubDataNode(api, fieldData = {}) {
   const {
@@ -36,36 +36,17 @@ async function createStaticGithubDataNode(api, fieldData = {}) {
 }
 
 module.exports = {
-  async createSchemaCustomization({ actions: { createTypes }, schema }) {
-    createTypes(
-      schema.buildObjectType({
-        name: 'StaticGithubData',
-        fields: {
-          stars: 'String'
-        }
-      })
-    )
-  },
-  async createResolvers({ createResolvers }) {
-    createResolvers({
-      Query: {
-        staticGithubData: {
-          type: 'StaticGithubData',
-          async resolve(source, args, context) {
-            const nodes = await context.nodeModel.getAllNodes({
-              type: 'GithubData'
-            })
-            const node = nodes[0]
-            return node
-              ? {
-                  stars: node.rawResult.data.repository.stargazers.totalCount
-                }
-              : {
-                  stars: 8888
-                }
-          }
-        }
-      }
+  sourceNodes: createStaticGithubDataNode,
+  async onCreateNode(api) {
+    const {
+      node,
+      actions: { createParentChildLink }
+    } = api
+    if (node.internal.type !== 'GithubData') return
+    const child = await createStaticGithubDataNode(api, {
+      parent: node.id,
+      stars: node.rawResult.data.repository.stargazers.totalCount
     })
+    await createParentChildLink({ child, parent: node })
   }
 }
