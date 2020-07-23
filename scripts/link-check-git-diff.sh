9256d831cbd32ca510defb3d610a1f4e86c97f69
rogermparent <rogermparent@gmail.com>
2020-07-23 18:03:40 -0400

    Adapt git link checker to use exit codes

--- scripts/link-check-git-diff.sh
+++ scripts/link-check-git-diff.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
 source "$(dirname "$0")"/utils.sh
-pushd "$repo"
 
 git fetch origin master
 
 differ="git diff $(git merge-base HEAD origin/master)"
 changed="$($differ --name-only -- '*.css' '*.js' '*.jsx' '*.md' '*.tsx' '*.ts' '*.json' ':!redirects-list.json' ':!*.test.js')"
+code=0
 
 [ -z "$changed" ] && exit 0
 
@@ -17,6 +15,9 @@ echo "$changed" | while read -r file ; do
   # check just changed lines
   echo -n "$file:"
   "$(dirname "$0")"/link-check.sh <($differ -U0 -- "$file" | grep '^\+')
+  if [ $? -ne 0 ] ; then
+     code=1
+  fi
 done
 
-popd
+exit $code
