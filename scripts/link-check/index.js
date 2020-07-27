fdb4b9d8e7d1e3df673c5977b4d8a9a9bde49d24
rogermparent <rogermparent@gmail.com>
2020-07-26 23:48:33 -0400

    Move Action to its own repo
    Considering the embedded deps requirement, it's best we just move this action to
    its own area to avoid complicating the original repo unnecessarily.

--- scripts/link-check/index.js
+++
@@ -1,8 +0,0 @@
-// CLI runner for link-check-git-diff
-const { getCheckedAddedLinks } = require('./link-check-git-diff.js')
-const formatLinks = require('./formatting.js')
-const baseURL = process.env.CHECK_LINKS_RELATIVE_URL
-
-getCheckedAddedLinks(baseURL).then(links => {
-  console.log(formatLinks(links))
-})
