4161910aa6264ae0d16c71a1861840efec747257
rogermparent <rogermparent@gmail.com>
2020-12-09 15:55:21 -0500

    Add simple Typeform component and /enterprise page using it

---
+++ src/pages/enterprise.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Typeform from '../components/Typeform'
+const EnterpriseTypeform: React.FC<{}> = () => (
+  <Typeform
+    url="https://iterative-ai.typeform.com/to/xGTxcID7"
+    title="Enterprise Form"
+  />
+)
+
+export default EnterpriseTypeform
