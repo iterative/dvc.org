e8e41ee5485370e1c00ed5f54abf8a78fcd22d21
Emre Sahin <github@emresult.com>
2021-02-17 10:41:57 +0300

    reorganize the  script

--- check-help-output.zsh
+++ check-help-output.zsh
@@ -14,6 +14,30 @@ fi
 
 $DVC --help > ${HELP_TEXT_DIR}/dvc-help.txt
 
+check_output_diff () {
+    cmd=$1
+    subcmd=$2
+    out_f=${HELP_TEXT_DIR}/${cmd}-${subcmd}-help.txt
+    echo $cmd $subcmd
+    $DVC ${cmd} ${subcmd} --help > ${out_f}
+    diff=$(git diff --ignore-all-space ${out_f})
+    if [[ -n "${diff}" ]] ; then
+        git diff ${out_f}
+        response=""
+        while [[ ( ("$response" != "y") && ("$response" != "n") ) ]] ; do
+            vared -p "Create Github issue for this change? [y/n]" response
+        done
+        if [[ "$response" == "y" ]] ; then
+            diff=$(git diff ${out_f} | tail -n +7)
+            the_body="The diff in the command output is\\n\`\`\`diff\\n${diff}\\n\`\`\`"
+            echo "$the_body"
+            gh issue create --title "cmd: [auto] changes in \`dvc ${cmd} ${subcmd} --help\` should be reflected to command reference" --body "${the_body}"
+        fi
+        # git add ${f}
+        # git commit -m "[auto] updated help output for ${f}"
+    fi
+}
+
 for d in ${COMMAND_REF_DIR}/*(/) ; do 
     for f in ${d}/*.md ; do 
         if [[ ${f:t} == "index.md" ]] ; then
@@ -21,8 +45,7 @@ for d in ${COMMAND_REF_DIR}/*(/) ; do
         fi
         cmd=${d:t}
         subcmd=${f:r:t}
-        echo $cmd $subcmd
-        $DVC ${cmd} ${subcmd} --help > ${HELP_TEXT_DIR}/${cmd}-${subcmd}-help.txt
+        check_output_diff $cmd $subcmd
     done
 done
 
@@ -31,25 +54,7 @@ for f in ${COMMAND_REF_DIR}/*.md ; do
         continue
     fi
     cmd=${f:r:t}
-    echo $cmd
-    $DVC ${cmd} --help > ${HELP_TEXT_DIR}/${cmd}-help.txt
+    check_output_diff $cmd
 done
 
-for f in ${HELP_TEXT_DIR}/*.txt ; do
-    diff=$(git diff ${f})
-    if [[ -n "${diff}" ]] ; then
-        git diff ${f}
-        response=""
-        while [[ $response = "y" || $response = "n" ]] ; do
-            vared -p "Create Github issue for this change? [y/n]" response
-        done
-        if [[ $response = "y" ]] ; then
-            gh issue create --title "cmd: [auto] changes in ${f} should be reflected to command reference" --body "The diff is \n```${diff}```"
-        fi
-        git add ${f}
-        git commit -m "[auto] updated help output for ${f}"
-    fi
-done
-    
-        
 
