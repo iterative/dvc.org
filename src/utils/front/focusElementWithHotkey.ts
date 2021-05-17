74a96c7297388e4b597fd853c7083a84dfe5f73b
evan-cohen <cohen.evan.b@gmail.com>
2021-05-17 21:32:57 +0000

    fix: add ts return type to focusElementWithHotkey

--- src/utils/front/focusElementWithHotkey.ts
+++ src/utils/front/focusElementWithHotkey.ts
@@ -3,7 +3,10 @@
  * @param target CSS selector for the element to be focused
  * @param hotkey The key that should trigger the target element to be focused
  */
-export const focusElementWithHotkey = (target: string, hotkey: string) => {
+export const focusElementWithHotkey = (
+  target: string,
+  hotkey: string
+): void => {
   document.addEventListener('keydown', $event => {
     if ($event.key !== hotkey || document.activeElement?.matches(target)) {
       return
