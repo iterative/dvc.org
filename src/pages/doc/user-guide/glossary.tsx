6bdea402d1afe8dabcc4a36ecb6edcd6c5394dfa
julieg18 <juliannagal18@gmail.com>
2021-07-01 07:21:04 -0500

    Use one dangerouslySetInnerHTML in glossary page

--- src/pages/doc/user-guide/glossary.tsx
+++ src/pages/doc/user-guide/glossary.tsx
@@ -5,20 +5,22 @@ import useGlossary from '../../../utils/front/glossary'
 
 const Glossary: React.FC = () => {
   const { contents } = useGlossary()
+  const html = contents
+    .sort((c1, c2) => {
+      const c1Name = c1.name.toLowerCase()
+      const c2Name = c2.name.toLowerCase()
+      return c1Name < c2Name ? -1 : c2Name < c1Name ? 1 : 0
+    })
+    .map(
+      ({ name, desc }: { name: string; desc: string }) =>
+        `<p><strong>${name}</strong>: ${desc.replace(/<p\/?>/, '')}</p>`
+    )
+    .join('')
 
   return (
     <DocWithJsx slug="/doc/user-guide/glossary" headings={[]}>
-      <h1>Glossary</h1>
-      {contents
-        .sort((c1, c2) => {
-          const c1Name = c1.name.toLowerCase()
-          const c2Name = c2.name.toLowerCase()
-          return c1Name < c2Name ? -1 : c2Name < c1Name ? 1 : 0
-        })
-        .map(({ name, desc }: { name: string; desc: string }, i: number) => {
-          const html = `<strong>${name}</strong>: ${desc.replace(/<p\/?>/, '')}`
-          return <p key={i} dangerouslySetInnerHTML={{ __html: html }} />
-        })}
+      <h1 id="glossary">Glossary</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </DocWithJsx>
   )
 }
