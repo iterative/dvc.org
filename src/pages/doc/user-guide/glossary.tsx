7edac9316ce2a5a8392744f2287bce72d8ff00bb
julieg18 <juliannagal18@gmail.com>
2021-07-01 17:05:45 -0500

    Convert AutoLinkHeader into AutoLinkElement

--- src/pages/doc/user-guide/glossary.tsx
+++ src/pages/doc/user-guide/glossary.tsx
@@ -1,27 +1,42 @@
 import React from 'react'
 
 import DocWithJsx from '../../../templates/doc-jsx'
-import AutoLinkHeader from '../../../components/Documentation/WithJSX/AutoLinkHeader'
+import AutoLinkElement from '../../../components/Documentation/WithJSX/AutoLinkElement'
 import useGlossary from '../../../utils/front/glossary'
 
 const Glossary: React.FC = () => {
   const { contents } = useGlossary()
-  const html = contents
-    .sort((c1, c2) => {
-      const c1Name = c1.name.toLowerCase()
-      const c2Name = c2.name.toLowerCase()
-      return c1Name < c2Name ? -1 : c2Name < c1Name ? 1 : 0
-    })
-    .map(
-      ({ name, desc }: { name: string; desc: string }) =>
-        `<p><strong>${name}</strong>: ${desc.replace(/<\/?p>/g, '')}</p>`
-    )
-    .join('')
 
   return (
     <DocWithJsx slug="/doc/user-guide/glossary" headings={[]}>
-      <AutoLinkHeader id="glossary">Glossary</AutoLinkHeader>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <AutoLinkElement el="h1" id="glossary">
+        Glossary
+      </AutoLinkElement>
+      {contents
+        .sort((c1, c2) => {
+          const c1Name = c1.name.toLowerCase()
+          const c2Name = c2.name.toLowerCase()
+          return c1Name < c2Name ? -1 : c2Name < c1Name ? 1 : 0
+        })
+        .map(({ name, desc }: { name: string; desc: string }, i: number) => (
+          <p key={i}>
+            <strong>
+              <AutoLinkElement
+                id={name
+                  .toLowerCase()
+                  .replace(/[^a-zA-Z0-9 ]/g, '')
+                  .replace(/ /g, '-')}
+                anchorStyle={{ paddingTop: '4px' }}
+              >
+                {name}
+              </AutoLinkElement>
+            </strong>
+            :{' '}
+            <span
+              dangerouslySetInnerHTML={{ __html: desc.replace(/<\/?p>/g, '') }}
+            ></span>
+          </p>
+        ))}
     </DocWithJsx>
   )
 }
