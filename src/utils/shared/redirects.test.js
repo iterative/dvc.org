94a2ef60d39bd7fa32c59bbdbda27918e558dcdc
rogermparent <rogermparent@gmail.com>
2021-08-24 18:12:59 -0400

    Remove one redundant test

--- src/utils/shared/redirects.test.js
+++ src/utils/shared/redirects.test.js
@@ -241,9 +241,8 @@ describe('getRedirects', () => {
   })
 
   describe('Does not accidentally redirect to an external site', () => {
-    itRedirects('https://dvc.org//google.com/', '/google.com')
     itRedirects('//google.com/', '/google.com')
-    itRedirects('///evil.com/', '/evil.com')
+    itRedirects('https://dvc.org//google.com/', '/google.com')
     itRedirects('/////////evil.com/', '/evil.com')
   })
 })
