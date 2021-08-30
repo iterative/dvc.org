639e0af8da07db694972569801bbf31e328a6137
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-08-30 17:28:12 +0200

    Added liveLinker

---
+++ plugins/gatsby-remark-dvc-linker/liveLinker.js
@@ -0,0 +1,28 @@
+/* eslint-env node */
+
+const { createLinkNode } = require('./helpers')
+const { getItemByPath } = require('../../src/utils/shared/sidebar')
+
+const LIVE_API_REGEXP = /dvclive.([a-z-._]*\(\)$)?/
+const METHOD_REGEXP = /^[a-z-._]*\(\)$/
+const API_ROOT = '/doc/dvclive/api-reference/'
+
+module.exports = astNode => {
+  const node = astNode[0]
+  const parent = astNode[2]
+
+  if (parent.type !== 'link' && LIVE_API_REGEXP.test(node.value)) {
+    const parts = node.value.split('.')
+
+    const isMethod = parts[2] && METHOD_REGEXP.test(parts[2])
+    const method = isMethod && parts[1].slice(0, -1)
+    const url = `${API_ROOT}${method}`
+
+    const isMethodPageExists = getItemByPath(url)
+    if (isMethodPageExists) {
+      createLinkNode(url, astNode)
+    }
+  }
+
+  return astNode
+}
