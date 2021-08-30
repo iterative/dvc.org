520acd8f91144aee1ed6a70fbaf2b28c98b3332e
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-08-30 17:57:27 +0200

    Added test

--- plugins/gatsby-remark-dvc-linker/liveLinker.js
+++ plugins/gatsby-remark-dvc-linker/liveLinker.js
@@ -13,9 +13,8 @@ module.exports = astNode => {
 
   if (parent.type !== 'link' && LIVE_API_REGEXP.test(node.value)) {
     const parts = node.value.split('.')
-
-    const isMethod = parts[2] && METHOD_REGEXP.test(parts[2])
-    const method = isMethod && parts[1].slice(0, -1)
+    const isMethod = parts[1] && METHOD_REGEXP.test(parts[1])
+    const method = isMethod && parts[1].slice(0, -2)
     const url = `${API_ROOT}${method}`
 
     const isMethodPageExists = getItemByPath(url)
