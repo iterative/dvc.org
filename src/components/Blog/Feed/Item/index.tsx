fd3fcee16b9fdf316278814a6191c90718ccd185
julieg18 <juliannagal18@gmail.com>
2021-09-06 07:41:29 -0500

    Reduce complexity in components/Blog/Feed/Item

--- src/components/Blog/Feed/Item/index.tsx
+++ src/components/Blog/Feed/Item/index.tsx
@@ -66,8 +66,8 @@ const Item: React.FC<IBlogFeedItemProps> = ({
       )}
     >
       <Link href={slug} className={styles.pictureLink}>
-        {picture ? (
-          image && <Image fluid={image} className={styles.picture} />
+        {image ? (
+          <Image fluid={image} className={styles.picture} />
         ) : (
           <Placeholder className={styles.picture} />
         )}
