d60ffd535418a66b0cf102c224bf6c35e382d54f
rogermparent <rogermparent@gmail.com>
2021-09-29 22:33:48 -0400

    Remove debug stuff and mkdir that caused issues

--- scripts/heroku-deploy.sh
+++ scripts/heroku-deploy.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 NEWPWD="/tmp/gatsby-build"
-mkdir $NEWPWD
 mv $OLDPWD $NEWPWD
 ln -s $NEWPWD $OLDPWD
-ls $OLDPWD
 cd $NEWPWD
-echo $NEWPWD
-ls
-ls ./scripts
-$NEWPWD/scripts/deploy-with-s3.js
+./scripts/deploy-with-s3.js
