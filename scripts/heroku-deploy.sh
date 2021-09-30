3a847ca93a588c51d0a16668edd8d7e7072bc5b4
rogermparent <rogermparent@gmail.com>
2021-09-29 22:14:03 -0400

    Remove echos and debug with which

--- scripts/heroku-deploy.sh
+++ scripts/heroku-deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 NEWPWD="/tmp/gatsby-build"
-echo $OLDPWD
-echo $NEWPWD
 mkdir $NEWPWD
 mv $OLDPWD $NEWPWD
 ln -s $NEWPWD $OLDPWD
 cd $NEWPWD
+which node
 node ./scripts/deploy-with-s3.js
