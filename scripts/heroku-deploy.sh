c71cb9ca7711b8052e7ba2f7331b1b9ccd31fa58
rogermparent <rogermparent@gmail.com>
2021-09-29 22:30:32 -0400

    Even more debugging

--- scripts/heroku-deploy.sh
+++ scripts/heroku-deploy.sh
@@ -3,8 +3,9 @@ NEWPWD="/tmp/gatsby-build"
 mkdir $NEWPWD
 mv $OLDPWD $NEWPWD
 ln -s $NEWPWD $OLDPWD
-echo $PATH
 ls $OLDPWD
 cd $NEWPWD
-which node
-./scripts/deploy-with-s3.js
+echo $NEWPWD
+ls
+ls ./scripts
+$NEWPWD/scripts/deploy-with-s3.js
