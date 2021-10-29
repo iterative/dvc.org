73450c2f19ec215a4d250703f754b612abc17da4
julieg18 <juliannagal18@gmail.com>
2021-10-29 06:28:41 -0500

    Update md tabs - update md tab styling - add tab query to url when tab is clicked - automatically open tab when corresponding tab query is in url

--- src/components/Documentation/Markdown/ToggleProvider/index.tsx
+++ src/components/Documentation/Markdown/ToggleProvider/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 interface ITogglesData {
   [key: string]: { texts: string[]; checkedInd: number }
@@ -12,12 +12,51 @@ interface ITogglesContext {
 
 export const TogglesContext = createContext<ITogglesContext>({})
 
+const makeTextUrlFriendly = (val: string): string =>
+  val.replace(/[^\w\-\._~]/g, '-').replace(/-+/g, '-')
+
+const getUrlQueryVal = (query: string, param: string): string => {
+  const params = new URLSearchParams(query)
+  return params.get(param) || ''
+}
+
+const setUrlQuery = (href: string, param: string, value: string): void => {
+  const formattedVal = makeTextUrlFriendly(value)
+  const url = new URL(href)
+  url.searchParams.set(param, formattedVal)
+  window.history.pushState({}, '', url.href)
+}
+
+const getSelectedIndexBasedOffQueryVal = (
+  texts: string[],
+  queryVal: string
+): number => {
+  const urlFriendlyTexts = texts.map(makeTextUrlFriendly)
+  const index = urlFriendlyTexts.indexOf(queryVal)
+  return index > -1 ? index : 0
+}
+
 export const TogglesProvider: React.FC = ({ children }) => {
   const [togglesData, setTogglesData] = useState({})
+  const [lastSelectedTab, setLastSelectedTab] = useState<null | string>(null)
+
+  useEffect(() => {
+    const tab = getUrlQueryVal(window.location.search, 'tab')
+    setLastSelectedTab(tab)
+  }, [])
 
   const addNewToggle = (id: string, texts: string[]): void => {
+    let lastSelected = lastSelectedTab
     const togglesDataCopy: ITogglesData = { ...togglesData }
-    togglesDataCopy[id] = { texts, checkedInd: 0 }
+
+    if (lastSelected === null) {
+      lastSelected = getUrlQueryVal(window.location.search, 'tab')
+    }
+
+    togglesDataCopy[id] = {
+      texts,
+      checkedInd: getSelectedIndexBasedOffQueryVal(texts, lastSelected)
+    }
     setTogglesData(togglesDataCopy)
   }
 
@@ -35,6 +74,8 @@ export const TogglesProvider: React.FC = ({ children }) => {
       }
     }
 
+    setUrlQuery(window.location.href, 'tab', selectedTabText)
+    setLastSelectedTab(selectedTabText)
     setTogglesData(togglesDataCopy)
   }
 
