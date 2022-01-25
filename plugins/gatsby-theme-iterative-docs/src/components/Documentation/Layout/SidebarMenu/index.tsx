d975f0298423ed25e7b26a1e46d73b5d448534e8
rogermparent <rogermparent@gmail.com>
2022-01-25 18:17:12 -0500

    Break apart docs sidebar and move it into theme

--- plugins/gatsby-theme-iterative-docs/src/components/Documentation/Layout/SidebarMenu/index.tsx
+++ plugins/gatsby-theme-iterative-docs/src/components/Documentation/Layout/SidebarMenu/index.tsx
@@ -1,166 +1,20 @@
-import React, { useEffect, useRef, useState, SyntheticEvent } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useLocation } from '@reach/router'
 import cn from 'classnames'
-import { Collapse } from 'react-collapse'
 import PerfectScrollbar from 'perfect-scrollbar'
-import includes from 'lodash/includes'
 
-import Link from 'gatsby-theme-iterative-docs/src/components/Link'
-import { ReactComponent as ExternalLinkIcon } from './external-link-icon.svg'
-import { ReactComponent as HouseIcon } from './house.svg'
-import { ReactComponent as CMLIcon } from './cml_bw_logo.svg'
-import { ReactComponent as StudioIcon } from './studio_gray_icon.svg'
-
-import {
-  structure,
-  getParentsListFromPath,
-  getPathWithSource
-} from 'gatsby-theme-iterative-docs/src/utils/shared/sidebar'
+import { getParentsListFromPath } from '../../../../utils/shared/sidebar'
 
 import 'perfect-scrollbar/css/perfect-scrollbar.css'
 import * as styles from './styles.module.css'
 
-// A map for optional special icons that can be used in menu items
-// Use the key string here as the "icon" field in sidebar.json
-const ICONS: { [key: string]: React.FC<{ className?: string }> } = {
-  house: HouseIcon,
-  cml: CMLIcon,
-  studio: StudioIcon
-}
+import InnerSidebar from './InnerSidebar'
 
-interface ISidebarMenuItemProps {
-  children?: Array<{ label: string; path: string; source: boolean | string }>
-  label: string
-  path: string
-  source: boolean | string
-  onClick: (isLeafItemClicked: boolean) => void
-  activePaths?: Array<string>
-  type?: string
-  style?: string
-  icon?: string
-}
-
-const SidebarMenuItem: React.FC<ISidebarMenuItemProps> = ({
-  children,
-  label,
-  path,
-  activePaths,
-  onClick,
-  style,
-  icon,
-  type
-}) => {
-  const [isExpanded, setIsExpanded] = useState(
-    activePaths && includes(activePaths, path)
-  )
-
-  useEffect(() => {
-    setIsExpanded(activePaths && includes(activePaths, path))
-  }, [activePaths])
-
-  const isRootParent =
-    activePaths && activePaths.length > 1 && activePaths[0] === path
-
-  const isLeafItem = children === undefined || children.length === 0
-
-  const currentLevelOnClick = (
-    event: SyntheticEvent<HTMLAnchorElement>
-  ): void => {
-    if (event.currentTarget.getAttribute('aria-current') === 'page') {
-      event.preventDefault()
-      setIsExpanded(!isExpanded)
-    }
-    onClick(isLeafItem)
-  }
-
-  const bulletIconClick = (event: SyntheticEvent<HTMLSpanElement>): void => {
-    event.preventDefault()
-    setIsExpanded(!isExpanded)
-  }
-
-  // Fetch a special icon if one is defined
-  const IconComponent = icon && ICONS[icon]
-  const iconElement = IconComponent ? (
-    <IconComponent className={styles.specialIcon} />
-  ) : null
-
-  const className = cn(
-    styles.sectionLink,
-    isExpanded && styles.active,
-    isRootParent && 'docSearch-lvl0',
-    'link-with-focus',
-    // style ? styles[style] : styles.sidebarDefault,
-    style && styles[style],
-    // isLeafItem && styles.leafItem,
-    // Limit the default bullet to items with no special icon
-    // icon ? undefined : styles.withDefaultBullet
-    icon && undefined
-  )
-
-  const bulletIconClassName = cn(
-    styles.sidebarDefaultBullet,
-    isExpanded && styles.active,
-    isLeafItem && styles.sidebarLeafBullet
-  )
-
-  const bulletIconJSX = isLeafItem ? (
-    <span className={bulletIconClassName}></span>
-  ) : (
-    <button onClick={bulletIconClick} className={bulletIconClassName}></button>
-  )
-
-  const parentElement =
-    type === 'external' ? (
-      <Link
-        href={path}
-        id={path}
-        className={className}
-        onClick={currentLevelOnClick}
-        target="_blank"
-      >
-        {iconElement ? (
-          iconElement
-        ) : (
-          <span className={bulletIconClassName}></span>
-        )}
-        {label} <ExternalLinkIcon />
-      </Link>
-    ) : (
-      <Link
-        href={getPathWithSource(path)}
-        id={path}
-        className={className}
-        onClick={currentLevelOnClick}
-      >
-        {iconElement ? iconElement : bulletIconJSX}
-        {label}
-      </Link>
-    )
-
-  return (
-    <>
-      {parentElement}
-      {children && (
-        <span hidden={!isExpanded}>
-          <Collapse isOpened={!!isExpanded}>
-            {children.map(item => (
-              <SidebarMenuItem
-                key={item.path}
-                activePaths={activePaths}
-                onClick={onClick}
-                {...item}
-              />
-            ))}
-          </Collapse>
-        </span>
-      )}
-    </>
-  )
-}
+export type SidebarItemClickHandler = (isLeafItemClicked: boolean) => void
 
 interface ISidebarMenuProps {
   currentPath: string
-  onClick: (isLeafItemClicked: boolean) => void
+  onClick: SidebarItemClickHandler
 }
 
 const SidebarMenu: React.FC<ISidebarMenuProps> = ({ currentPath, onClick }) => {
@@ -218,20 +72,7 @@ const SidebarMenu: React.FC<ISidebarMenuProps> = ({ currentPath, onClick }) => {
       className={cn(styles.menu, isScrollHidden && styles.isScrollHidden)}
       ref={rootRef}
     >
-      <div className={styles.sections}>
-        <div className={styles.sectionLinks}>
-          {structure.map(item => (
-            <SidebarMenuItem
-              key={item.path}
-              activePaths={
-                includes(activePaths, item.path) ? activePaths : undefined
-              }
-              onClick={onClick}
-              {...item}
-            />
-          ))}
-        </div>
-      </div>
+      <InnerSidebar onClick={onClick} activePaths={activePaths} />
     </div>
   )
 }
