083f0655b98bfb193a1fd96e00a4cb256328af9e
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-01-26 18:17:55 +0100

    Remove hardcoded test

--- plugins/gatsby-remark-dvctable-filler/index.test.js
+++ plugins/gatsby-remark-dvctable-filler/index.test.js
@@ -1,4 +1,5 @@
 const gatsbyRemarkDvctableFiller = require('.')
+const tables = require('./tables.js')
 
 const { buildAst } = require('../gatsby-remark-dvc-linker/helpers.js')
 
@@ -6,10 +7,11 @@ describe('gatsby-remark-dvctable-filler', () => {
   it('fills dvctable', () => {
     const ast = buildAst('```dvctable\n$get-started-exp-show-combined\n```')
     gatsbyRemarkDvctableFiller({ markdownAST: ast })
+    let table = tables.find(
+      o => o.placeholder === '$get-started-exp-show-combined'
+    )
     expect(ast).toEqual(
-      buildAst(
-        '```dvctable\n ───────────────────────────────────────────────────────────────────────────────────────────────────────── \n  neutral:**Experiment**                  metric:**avg_prec**   metric:**roc_auc**   param:**featurize.max_features**   param:**train.n_est**   param:**train.min_split**  \n ───────────────────────────────────────────────────────────────────────────────────────────────────────── \n  workspace                    0.60405    0.9608   3000                     100           64               \n  random-forest-experiments    0.60405    0.9608   3000                     100           64               \n  ├── 88ecb7e [exp-8260f]      0.51799   0.92333   500                      100           64               \n  ├── e720fcc [exp-0182d]      0.58589     0.945   2000                     100           64               \n  └── 2de2f53 [exp-540e3]      0.55669   0.93516   1000                     100           64               \n ───────────────────────────────────────────────────────────────────────────────────────────────────────── \n\n```'
-      )
+      buildAst('```dvctable\n' + `${table.replacement}` + '\n```')
     )
   })
 })
