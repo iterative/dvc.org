d46162d0d07f41bdac70c3f262db87e6f45e08d2
Thomas Kunwar <yathomasi@gmail.com>
2022-01-28 15:33:35 +0545

    Revert "added git datetime field"
    This reverts commit ee00131f709241c949363b525925105cb40cb9c1.

--- src/gatsby/models/blog/onCreateMarkdownContentNode.js
+++ src/gatsby/models/blog/onCreateMarkdownContentNode.js
@@ -1,11 +1,9 @@
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
 const { markdownToHtml } = require('../../common.js')
 
 async function createMarkdownBlogNode(api, { parentNode, createChildNode }) {
   if (parentNode.relativeDirectory.split('/')[0] !== 'blog') return
   const { node, createNodeId, createContentDigest } = api
-  const { frontmatter, rawMarkdownBody, fileAbsolutePath } = node
+  const { frontmatter, rawMarkdownBody } = node
   const {
     date,
     tags,
@@ -19,9 +17,6 @@ async function createMarkdownBlogNode(api, { parentNode, createChildNode }) {
   } = frontmatter
   const { name, relativePath } = parentNode
 
-  const { stdout: gitDateTime } = await exec(
-    `git log -1 --pretty=format:%aI ${fileAbsolutePath}`
-  )
   const slug = /[-\d]*(.*)/.exec(name)[1]
 
   const pagePath = '/blog/' + slug
@@ -37,8 +32,7 @@ async function createMarkdownBlogNode(api, { parentNode, createChildNode }) {
     commentsUrl,
     picture,
     pictureComment: markdownToHtml(pictureComment),
-    sourcePath: relativePath,
-    gitDateTime
+    sourcePath: relativePath
   }
   const postNode = {
     ...fieldData,
