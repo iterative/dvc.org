c88e09e9c89ec18b42a7350128ea86b82492e6b5
julieg18 <juliannagal18@gmail.com>
2022-02-03 15:43:18 -0600

    Add remove click listeners

--- plugins/gatsby-theme-iterative-docs/src/utils/front/useCustomYtEmbeds.ts
+++ plugins/gatsby-theme-iterative-docs/src/utils/front/useCustomYtEmbeds.ts
@@ -2,26 +2,44 @@ import { useEffect } from 'react'
 
 const setUpCustomYtEmbeds = () => {
   const ytEmbeds = document.querySelectorAll('.yt-embed-wrapper')
+  const removeClickListeners: Array<() => void> = []
+
   ytEmbeds.forEach(embed => {
     const iframe = embed.querySelector('iframe')
     const overlay = embed.querySelector('.yt-embed-wrapper__overlay')
     const tooltip = embed.querySelector('.yt-embed-wrapper__tooltip')
 
-    overlay?.addEventListener('click', event => {
+    const handleOverlayClick = (event: MouseEvent) => {
       if (event.target === tooltip || tooltip?.contains(event.target as Node)) {
         return
       }
       if (iframe && iframe.src) {
         iframe.src = iframe?.src + `&autoplay=1`
       }
-      overlay.classList.add('hidden')
-    })
+      overlay?.classList.add('hidden')
+    }
+
+    const removeListener = () => {
+      overlay?.removeEventListener('click', handleOverlayClick as EventListener)
+    }
+
+    overlay?.addEventListener('click', handleOverlayClick as EventListener)
+
+    removeClickListeners.push(removeListener)
   })
+
+  return () => {
+    removeClickListeners.forEach(rmListener => rmListener())
+  }
 }
 
 const useCustomYtEmbeds = () => {
   useEffect(() => {
-    setUpCustomYtEmbeds()
+    const removeEventListeners = setUpCustomYtEmbeds()
+
+    return () => {
+      removeEventListeners()
+    }
   }, [])
 }
 
