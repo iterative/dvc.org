e0fce2c3c0e94a0a9eb47ca8939632ce23a9baa2
Thomas Kunwar <yathomasi@gmail.com>
2022-02-14 22:47:03 +0545

    updated with default values

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 const config = {
   algolia: {
-    appId: String(process.env.GATSBY_ALGOLIA_APP_ID),
-    searchKey: String(process.env.GATSBY_ALGOLIA_SEARCH_KEY),
+    appId: String(process.env.GATSBY_ALGOLIA_APP_ID) || 'B87HVF62EF',
+    searchKey:
+      String(process.env.GATSBY_ALGOLIA_SEARCH_KEY) ||
+      'cd45432547aa825db0ab63f4d90e13e3',
     adminKey: process.env.ALGOLIA_ADMIN_KEY,
-    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME || 'dev_blogs'
+    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME || 'dvc_blogs'
   }
 }
 
