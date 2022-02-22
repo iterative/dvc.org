c8ee72dd47495730fdab52d5e42e348241b0ab9e
Thomas Kunwar <yathomasi@gmail.com>
2022-02-22 23:05:13 +0545

    Search blogs  (#3198)
    * added gatsby plugin algolia and setup indexing
    
    * updated env variables
    
    * added search to blog
    
    * updated UI for search results
    
    * updated types
    
    * updated the query
    
    * added git datetime field
    
    * added paritial update for algolia
    
    * skip indexing if no env variables found
    
    * updated useEffect dependency and overflow y
    
    * updated the UI
    
    * Revert "added git datetime field"
    
    This reverts commit ee00131f709241c949363b525925105cb40cb9c1.
    
    * reverted partial update changes
    
    * fix for ios
    
    * Search blogs indexing on Github actions CI (#3264)
    
    * added script to build on ci
    
    * minor change
    
    * fix typo
    
    * updated skip indexing and minor fixs
    
    * added yarn caching
    
    * added caching and incremntal build
    
    * run only on blogs content change
    
    * added a test blog
    
    * removed test blog
    
    * added back paritial indexing for blogs
    
    * set gitDateTime only on CI
    
    * updated ci
    
    * updated with default values
    
    * added a manual tirgger
    
    * added a test blog
    
    * check only admin key
    
    * removed test blog
    
    * fixed the env values position
    
    * set default appid
    
    * minor fixs
    
    * caching .cache and public with same key
    
    * set branch to master
    
    * added input for full indexing as well

---
+++ src/gatsby/hooks/useClickOutside.tsx
@@ -0,0 +1,28 @@
+import { RefObject, useEffect } from 'react'
+
+const events = [`mousedown`, `touchstart`]
+
+const useClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  onClickOutside: (event: Event) => void
+) => {
+  const isOutside = (element: Node | null) =>
+    !ref.current || !ref.current.contains(element)
+
+  const onClick = (event: Event) => {
+    if (isOutside(event.target as Node)) {
+      onClickOutside(event)
+    }
+  }
+
+  useEffect(() => {
+    for (const event of events) {
+      document.addEventListener(event, onClick)
+    }
+
+    return () => {
+      for (const event of events) document.removeEventListener(event, onClick)
+    }
+  }, [ref, onClickOutside])
+}
+export default useClickOutside
