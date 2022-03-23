12526a11659e858f9e3aca9fd0cf416756c6f486
julieg18 <juliannagal18@gmail.com>
2022-03-23 09:54:22 -0500

    Check for unknown icons

--- plugins/gatsby-theme-iterative-docs/src/components/Documentation/Markdown/Admonition/index.tsx
+++ plugins/gatsby-theme-iterative-docs/src/components/Documentation/Markdown/Admonition/index.tsx
@@ -2,6 +2,22 @@ import React from 'react'
 import cn from 'classnames'
 import * as styles from './styles.module.css'
 
+const icons = {
+  tip: '💡',
+  info: 'ℹ️',
+  warn: '⚠️',
+  fire: '🔥',
+  exclamation: '❗',
+  lady_beetle: '🐞',
+  bug: '🐛'
+}
+const genericTitles = {
+  info: 'Info',
+  tip: 'Tip',
+  warn: 'Warning'
+}
+const defaultType = 'info'
+
 const Admonition: React.FC<{
   title?: string
   type?: 'info' | 'tip' | 'warn'
@@ -13,29 +29,18 @@ const Admonition: React.FC<{
     | 'exclamation'
     | 'lady_beetle'
     | 'bug'
-}> = ({ title, type = 'info', children, icon = type }) => {
-  const icons = {
-    tip: '💡',
-    info: 'ℹ️',
-    warn: '⚠️',
-    fire: '🔥',
-    exclamation: '❗',
-    lady_beetle: '🐞',
-    bug: '🐛'
-  }
-  const genericTitles = {
-    info: 'Info',
-    tip: 'Tip',
-    warn: 'Warning'
-  }
-  const setType = genericTitles[type] ? type : 'info'
+}> = ({ title, type = defaultType, children, icon = type }) => {
+  const setType = genericTitles[type] ? type : defaultType
+  const iconContent = icons[icon] || ''
 
   return (
     <div
       className={cn(styles.admonition, styles[setType])}
-      style={{ '--icon': `"${icons[icon] || ''}"` } as React.CSSProperties}
+      style={{ '--icon': `"${iconContent}"` } as React.CSSProperties}
     >
-      <p className={styles.title}>{title || genericTitles[setType]}</p>
+      <p className={cn(styles.title, !iconContent && styles.noIcon)}>
+        {title || genericTitles[setType]}
+      </p>
       <div className={styles.content}>{children}</div>
     </div>
   )
