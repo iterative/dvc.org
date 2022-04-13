3f3dd55ab15300459896a11979eb911664f08afd
Thomas Kunwar <yathomasi@gmail.com>
2022-04-13 09:45:44 +0545

    Added id as props for unique anchor (#3413)
    * added id as props for unique link
    
    * updated slugger
    
    * updated content
    
    * updated usememo
    
    * Apply suggestions from code review
    
    Co-authored-by: Jorge Orpinel <jorgeorpinel@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Jorge Orpinel <jorgeorpinel@users.noreply.github.com>
    
    Co-authored-by: Jorge Orpinel <jorgeorpinel@users.noreply.github.com>

---
+++ plugins/gatsby-theme-iterative-docs/src/utils/front/Slugger.ts
@@ -0,0 +1,37 @@
+class Slugger {
+  separator: string
+  lowercase: boolean
+  slugs: Array<string>
+
+  constructor(options?: { separator?: string; lowercase?: boolean }) {
+    this.separator = options?.separator || '-'
+    this.lowercase = options?.lowercase === false ? false : true
+    this.slugs = []
+  }
+
+  slug(str: string) {
+    str = typeof str === 'string' ? str : ''
+    let slug = this.slugify(str)
+
+    if (this.lowercase) {
+      slug = slug.toLowerCase()
+    }
+    if (this.slugs.includes(slug)) {
+      throw new Error(`Duplicate slug: ${slug}`)
+    }
+    this.slugs.push(slug)
+    return slug
+  }
+  slugify(str: string) {
+    return str
+      .replace(/[^\w\s-]/g, '')
+      .trim()
+      .replace(/[-\s]+/g, this.separator)
+      .replace(this.separator + this.separator, this.separator)
+      .replace(/(^\-+|\-+$)/g, '')
+  }
+  reset() {
+    this.slugs = []
+  }
+}
+export default Slugger
