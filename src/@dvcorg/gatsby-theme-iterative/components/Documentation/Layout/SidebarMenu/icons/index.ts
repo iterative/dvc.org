8b5e7224133dafa63d0028de1b55b03f2f108b8c
julieg18 <juliannagal18@gmail.com>
2022-06-14 15:50:58 -0500

    Add gray icon

--- src/@dvcorg/gatsby-theme-iterative/components/Documentation/Layout/SidebarMenu/icons/index.ts
+++ src/@dvcorg/gatsby-theme-iterative/components/Documentation/Layout/SidebarMenu/icons/index.ts
@@ -3,7 +3,7 @@ import defaultIcons, {
 } from '@dvcorg/gatsby-theme-iterative/src/components/Documentation/Layout/SidebarMenu/icons'
 import { ReactComponent as CMLIcon } from './cml-icon.svg'
 import { ReactComponent as StudioIcon } from './studio-icon.svg'
-import { ReactComponent as VsCodeIcon } from '../../../../../../../../static/img/vscode-icon.svg'
+import { ReactComponent as VsCodeIcon } from './vscode-icon.svg'
 
 const customizedIcons: SidebarIcons = {
   ...defaultIcons,
