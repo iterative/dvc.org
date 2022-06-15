9112055f6af940189fb5ad756d1212787b3a74a9
Julie <43496356+julieg18@users.noreply.github.com>
2022-06-15 07:32:36 -0500

    Add "vscode" icon to docs sidebar (#3666)

--- src/@dvcorg/gatsby-theme-iterative/components/Documentation/Layout/SidebarMenu/icons/index.ts
+++ src/@dvcorg/gatsby-theme-iterative/components/Documentation/Layout/SidebarMenu/icons/index.ts
@@ -3,11 +3,13 @@ import defaultIcons, {
 } from '@dvcorg/gatsby-theme-iterative/src/components/Documentation/Layout/SidebarMenu/icons'
 import { ReactComponent as CMLIcon } from './cml-icon.svg'
 import { ReactComponent as StudioIcon } from './studio-icon.svg'
+import { ReactComponent as VsCodeIcon } from './vscode-icon.svg'
 
 const customizedIcons: SidebarIcons = {
   ...defaultIcons,
   cml: CMLIcon,
-  studio: StudioIcon
+  studio: StudioIcon,
+  vscode: VsCodeIcon
 }
 
 export default customizedIcons
