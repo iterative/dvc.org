1b0f06a37bb3779b191ecfa4c6da563ef85c4803
efiop <efiop@users.noreply.github.com>
2022-07-28 18:04:05 +0000

    dvc 2.15.0

--- src/components/DownloadButton/index.tsx
+++ src/components/DownloadButton/index.tsx
@@ -9,7 +9,7 @@ import { logEvent } from '@dvcorg/gatsby-theme-iterative/src/utils/front/plausib
 import * as styles from './styles.module.css'
 import { OS, useUserOS } from '../../utils/front/useUserOS'
 
-const VERSION = `2.12.0`
+const VERSION = `2.15.0`
 
 const dropdownItems = [
   OS.UNKNOWN,
