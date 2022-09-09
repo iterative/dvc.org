9ad17388de8f6404bd84afcb87ab442c595be0cb
Thomas Kunwar <yathomasi@gmail.com>
2022-09-09 14:52:40 +0545

    use Script API

--- src/@dvcorg/gatsby-theme-iterative/components/Documentation/Layout/SearchForm/index.tsx
+++ src/@dvcorg/gatsby-theme-iterative/components/Documentation/Layout/SearchForm/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import Promise from 'promise-polyfill'
 import { loadResource } from '@dvcorg/gatsby-theme-iterative/src/utils/front/resources'
 
 import * as styles from '@dvcorg/gatsby-theme-iterative/src/components/Documentation/Layout/SearchForm/styles.module.css'
+import { Script } from 'gatsby'
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -13,29 +13,13 @@ declare global {
 
 const SearchForm: React.FC = props => {
   const [searchAvailable, setSearchAvailable] = useState<boolean>(false)
+  const [scriptLoaded, setScriptLoaded] = useState(false)
   useEffect(() => {
-    if (window) {
-      if (!window.docsearch) {
-        Promise.all([
-          loadResource(
-            'https://cdn.jsdelivr.net/npm/docsearch.js@2.6.3/dist/cdn/docsearch.min.css'
-          ),
-          loadResource(
-            'https://cdn.jsdelivr.net/npm/docsearch.js@2.6.3/dist/cdn/docsearch.min.js'
-          )
-        ]).then(() => {
-          if (window.docsearch) {
-            window.docsearch({
-              appId: '98DVTFT919',
-              apiKey: '38e749a9a63356b1485bff02afe466af',
-              indexName: 'dvc',
-              inputSelector: '#doc-search',
-              debug: false // Set to `true` if you want to inspect the dropdown
-            })
-            setSearchAvailable(true)
-          }
-        })
-      } else {
+    async function loadDocsearch() {
+      if (scriptLoaded && window && window.docsearch) {
+        await loadResource(
+          'https://cdn.jsdelivr.net/npm/docsearch.js@2.6.3/dist/cdn/docsearch.min.css'
+        )
         window.docsearch({
           appId: '98DVTFT919',
           apiKey: '38e749a9a63356b1485bff02afe466af',
@@ -46,21 +30,29 @@ const SearchForm: React.FC = props => {
         setSearchAvailable(true)
       }
     }
-  }, [])
+
+    loadDocsearch()
+  }, [scriptLoaded])
 
   return (
-    <div className={styles.searchArea}>
-      <div className={styles.container}>
-        <input
-          className={styles.input}
-          type="text"
-          id="doc-search"
-          placeholder="Search docs"
-          disabled={!searchAvailable}
-          {...props}
-        />
+    <>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/docsearch.js@2.6.3/dist/cdn/docsearch.min.js"
+        onLoad={() => setScriptLoaded(true)}
+      />
+      <div className={styles.searchArea}>
+        <div className={styles.container}>
+          <input
+            className={styles.input}
+            type="text"
+            id="doc-search"
+            placeholder="Search docs"
+            disabled={!searchAvailable}
+            {...props}
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
