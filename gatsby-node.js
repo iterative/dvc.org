38f8199e588cf5549c81319578b500c6554a4f07
rogermparent <rogermparent@gmail.com>
2022-12-16 18:54:40 -0500

    Remove webpack overrides that broke build

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,27 +17,3 @@ exports.onPostBuild = api => callOnModels(models, 'onPostBuild', api)
 exports.onCreatePage = ({ page, actions }) => {
   setPageContext(page, actions)
 }
-
-// Ignore warnings about CSS inclusion order, because we use CSS modules.
-// https://spectrum.chat/gatsby-js/general/having-issue-related-to-chunk-commons-mini-css-extract-plugin~0ee9c456-a37e-472a-a1a0-cc36f8ae6033?m=MTU3MjYyNDQ5OTAyNQ==
-exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  if (stage === 'build-javascript') {
-    const config = getConfig()
-
-    // Add polyfills
-    config.entry.app = [
-      'promise-polyfill/src/polyfill',
-      'isomorphic-fetch',
-      'raf-polyfill',
-      ...config.entry.app
-    ]
-
-    const miniCssExtractPlugin = config.plugins.find(
-      plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
-    )
-    if (miniCssExtractPlugin) {
-      miniCssExtractPlugin.options.ignoreOrder = true
-    }
-    actions.replaceWebpackConfig(config)
-  }
-}
