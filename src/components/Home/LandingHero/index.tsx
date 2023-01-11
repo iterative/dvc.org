d15051985e39f6868ae5f8e9196810d7ee3e139c
rogermparent <rogermparent@gmail.com>
2023-01-10 21:24:22 -0500

    Re-add download and get started buttons

--- src/components/Home/LandingHero/index.tsx
+++ src/components/Home/LandingHero/index.tsx
@@ -3,6 +3,25 @@ import cn from 'classnames'
 import { MemoizedTypedTerminal } from './Typed'
 import { graphql, useStaticQuery } from 'gatsby'
 
+import TwoRowsButtonLink from '../../TwoRowsButton/link'
+import GithubLine from './GithubLine'
+import DownloadButton from '../../DownloadButton'
+
+import * as styles from './styles.module.css'
+import { logEvent } from '@dvcorg/gatsby-theme-iterative/src/utils/front/plausible'
+import ShowOnly from '@dvcorg/gatsby-theme-iterative/src/components/ShowOnly'
+import Link from '@dvcorg/gatsby-theme-iterative/src/components/Link'
+
+const logUseCasesEvent = () => {
+  logEvent('Button', { Item: 'how-it-works' })
+}
+
+interface ILandingPageSlide {
+  title: string
+  description: string
+  terminal: string
+}
+
 const LandingHero = () => {
   const {
     landingPage: { slides }
@@ -16,7 +35,11 @@ const LandingHero = () => {
         }
       }
     }
-  `)
+  `) as {
+    landingPage: {
+      slides: ILandingPageSlide[]
+    }
+  }
 
   const [{ currentIndex }, changeCurrentIndex] = useReducer<
     Reducer<{ currentIndex: number; paused: boolean }, number | undefined>
@@ -134,6 +157,40 @@ const LandingHero = () => {
             )
           })}
         </ul>
+      </div>
+      <div className={styles.buttonsContainer}>
+        <ShowOnly on="mobile">
+          <Link
+            className={cn(styles.actionButton, styles.getStartedButton)}
+            href="/doc/start"
+          >
+            Get started
+          </Link>
+        </ShowOnly>
+        <ShowOnly on="desktop">
+          <DownloadButton />
+        </ShowOnly>
+        <TwoRowsButtonLink
+          mode="outline"
+          className={`${cn(
+            styles.actionButton,
+            styles.watchVideo
+          )} btn-with-focus btn-with-focus--white`}
+          title="Watch video"
+          description="How it works"
+          icon={
+            <img
+              className={styles.actionButtonIcon}
+              src="/img/play-icon.svg"
+              alt="Watch video"
+            />
+          }
+          onClick={logUseCasesEvent}
+          href="#use-cases"
+        />
+      </div>
+      <div className={styles.github}>
+        <GithubLine />
       </div>
     </div>
   )
