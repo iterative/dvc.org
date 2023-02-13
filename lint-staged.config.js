ad1e8635c3250ae1be249a465bebb52a656eb74e
rogermparent <rogermparent@gmail.com>
2023-02-13 12:38:17 -0500

    Tweak lint-staged config and make restyled config based from it

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  '*.css': ['prettier --write', 'stylelint --fix'],
-  '*.json': 'prettier --write',
-  '*.md': 'prettier --write',
-  '*.mdx': 'prettier --write',
   '*.ts?(x)': () => 'yarn lint-ts',
+  '*.{json,md,mdx,yml,yaml}': 'prettier --write',
+  '*.css': ['prettier --write', 'stylelint --fix'],
   '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix']
 }
