5e7fe5f703f655b4b7c0ea0840ce4daf0f726534
Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>
2023-04-12 11:12:51 +0545

    Animated logo carousel (#4459)

---
+++ src/components/Home/LogosSlider/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import cn from 'classnames'
+
+import * as styles from './styles.module.css'
+import CompanyLogos from './CompanyLogos'
+
+const CompanySlider = () => {
+  return (
+    <div className="mt-10">
+      <div className="mb-2">
+        <h2 className={cn('text-2xl font-medium text-center', styles.title)}>
+          Empowering thousands of users and customers from startups to Fortune
+          500 companies
+        </h2>
+      </div>
+      <div
+        className={cn(
+          'mb-2 w-full grid place-items-center overflow-hidden relative',
+          styles.fade
+        )}
+      >
+        <div className="flex justify-between gap-8 animate-slide">
+          <CompanyLogos />
+          <CompanyLogos />
+          <CompanyLogos />
+          <CompanyLogos />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CompanySlider
