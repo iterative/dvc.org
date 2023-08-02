f9ce3586fad1db0767364f2048a35bbffa8085fe
Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>
2023-08-03 01:08:12 +0545

    remove the how it works button

--- src/components/Home/Hero/GetStarted/GetStartedWithDvc.tsx
+++ src/components/Home/Hero/GetStarted/GetStartedWithDvc.tsx
@@ -15,10 +15,6 @@ import { logEvent } from '@dvcorg/gatsby-theme-iterative/src/utils/front/plausib
 import ShowOnly from '@dvcorg/gatsby-theme-iterative/src/components/ShowOnly'
 import Link from '@dvcorg/gatsby-theme-iterative/src/components/Link'
 
-const logUseCasesEvent = () => {
-  logEvent('Button', { Item: 'how-it-works' })
-}
-
 const logVSCodeEvent = () => {
   logEvent('Button', { Item: 'vscode-dvc-ext' })
 }
@@ -50,25 +46,6 @@ const HeroButtons = () => (
       }
       onClick={logVSCodeEvent}
       href="https://marketplace.visualstudio.com/items?itemName=Iterative.dvc"
-    />
-    <TwoRowsButtonLink
-      mode="outline"
-      className={cn(
-        styles.actionButton,
-        'btn-with-focus',
-        'btn-with-focus--white'
-      )}
-      title="Watch video"
-      description="How it works"
-      icon={
-        <img
-          className={styles.actionButtonIcon}
-          src="/img/play-icon.svg"
-          alt="Watch video"
-        />
-      }
-      onClick={logUseCasesEvent}
-      href="#use-cases"
     />
   </div>
 )
