35e27dd02e95ac6b90723f05ef83dd05a2528396
Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>
2023-08-29 09:03:34 +0545

    link to enterprise site - cloud.dvc.ai (#4811)

--- src/@dvcorg/gatsby-theme-iterative/components/LayoutHeader/Nav/index.tsx
+++ src/@dvcorg/gatsby-theme-iterative/components/LayoutHeader/Nav/index.tsx
@@ -4,9 +4,12 @@ import SocialIcons from './SocialIcons'
 import LinkItems from './LinkItems'
 
 import { logEvent } from '@dvcorg/gatsby-theme-iterative/src/utils/front/plausible'
+import { ReactComponent as ExternalLinkIcon } from '../../../../../../static/img/external-link-icon.svg'
 
 import * as styles from './styles.module.css'
 import PseudoButton from '@dvcorg/gatsby-theme-iterative/src/components/PseudoButton'
+import Link from '@dvcorg/gatsby-theme-iterative/src/components/Link'
+import { cloudUrl } from '../../../../../utils/externalUrls'
 
 const Nav: React.FC = () => (
   <div className={styles.wrapper}>
@@ -20,6 +23,13 @@ const Nav: React.FC = () => (
     >
       Get Started
     </PseudoButton>
+    <div className="text-dark hidden md:flex items-center">
+      <div className="mx-3 select-none">|</div>
+      <Link className="no-underline hover" href={cloudUrl}>
+        Get Enterprise{` `}
+        <ExternalLinkIcon className="ml-0.5 inline-block w-4 h-4" />
+      </Link>
+    </div>
   </div>
 )
 
