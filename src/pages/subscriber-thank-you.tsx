2f7bf762fd8b3ce83e9155d5a6aa6e41f511f235
Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>
2023-12-06 17:49:40 +0545

    hide alert banner

--- src/pages/subscriber-thank-you.tsx
+++ src/pages/subscriber-thank-you.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PageProps } from 'gatsby'
-import MainLayout from '@dvcorg/gatsby-theme-iterative/src/components/MainLayout'
+import MainLayout from '../components/MainLayout'
 import ThankYou from '../components/ThankYou'
 
 const ThankYouPage = ({ location }: PageProps) => (
