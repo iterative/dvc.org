042ea831a5d5a8728cf44e9405659fe329bcc8d2
Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>
2024-02-19 22:43:43 +0545

    feat: add platinum service support page

--- src/components/Support/index.tsx
+++ src/components/Support/index.tsx
@@ -1,133 +1,289 @@
 import React from 'react'
-import cn from 'classnames'
 
 import PageContent from '../PageContent'
 import LayoutWidthContainer from '@dvcorg/gatsby-theme-iterative/src/components/LayoutWidthContainer'
-import ShowOnly from '@dvcorg/gatsby-theme-iterative/src/components/ShowOnly'
+
 import HeroContainer from '../HeroContainer'
 import PromoSection from '../PromoSection'
 import Link from '@dvcorg/gatsby-theme-iterative/src/components/Link'
-import Popover from './Popover'
 
 import * as styles from './styles.module.css'
+import { BrainIcon, NotebookPenIcon, RouteIcon, TargetIcon } from 'lucide-react'
+import { cn } from '../../utils'
+
+const services = [
+  {
+    icon: BrainIcon,
+    title: 'MLOps and DVC experts',
+    points: [
+      <>
+        5+ years <span>experience</span>.
+      </>,
+      <>
+        Strong MLOps <span>technical</span> background.
+      </>,
+      <>
+        <span>Hands on</span> implementation and development.
+      </>
+    ]
+  },
+
+  {
+    icon: RouteIcon,
+    title: 'Coverage for all MLOps operations',
+    points: [
+      <>
+        <span>End to end solution: </span>
+        Data version control, project architecture, data pipelines, and data
+        curation
+      </>,
+      <>
+        By removing your teams from toil-laden work, you{' '}
+        <span>accelerate model development</span> and{' '}
+        <span>project delivery</span>.
+      </>
+    ]
+  },
+  {
+    icon: NotebookPenIcon,
+    title: 'Project planning and execution',
+    points: [
+      <>
+        Assist with <span>design</span> and <span>architecture</span>.
+      </>,
+      <>
+        Help align projects to <span>platform capabilities</span>.
+      </>,
+      <>
+        {' '}
+        Provide inputs to <span>project timelines</span> and{' '}
+        <span>resource needs</span>.
+      </>
+    ]
+  },
+  {
+    icon: TargetIcon,
+    title: 'Best practices and standards',
+    points: [
+      <>
+        <span>Architect</span> solutions for scalability.
+      </>,
+      <>
+        <span>Develop</span> common standards and frameworks.
+      </>,
+      <>
+        <span>Assist</span> with prototype development.
+      </>
+    ]
+  }
+]
+
+const colors = ['text-purple', 'text-blue', 'text-orange']
+
+const getAccentColor = (index: number) => {
+  return colors[index % colors.length]
+}
+
+const getSpanAccentColor = (index: number) => {
+  const spanColors = [
+    '[&>span:nth-child(3n-2)]:text-purple [&>span:nth-child(3n-1)]:text-blue [&>span:nth-child(3n)]:text-orange',
+    '[&>span:nth-child(3n-2)]:text-blue [&>span:nth-child(3n-1)]:text-orange [&>span:nth-child(3n)]:text-purple',
+    '[&>span:nth-child(3n-2)]:text-orange [&>span:nth-child(3n-1)]:text-purple [&>span:nth-child(3n)]:text-blue'
+  ]
+  return spanColors[index % colors.length]
+}
+
+const successStories = [
+  {
+    company: 'UBS',
+    logo: '/logos/ubs.png',
+    logoClassName: 'invert',
+    summary: `Iterative is a strategic partner for the AI element of UBS’s in-house data platform. Our
+    team is being leveraged to design a standardized ML development environment with full
+    reproducibility and audibility in mind. The project scope went from 6 months to 1 month.`
+  },
+  {
+    company: 'Walmart',
+    logo: '/logos/walmart.svg',
+    logoClassName: 'w-48 object-cover',
+    summary: `In addition the DVCx platform being instrumental in Walmart’s GenAI pipelines, Walmart
+    leverages Iterative Platinum Services to integrate their entire MLOps pipeline and get
+    support for their specific ML use cases to accelerate adoption and development speed.`
+  },
+  {
+    company: 'Omnisens',
+    logo: 'https://via.placeholder.com/150',
+    summary: `Omnisens was earlier in their ML journey when they started to engage the Iterative
+    Platinum Services team. Because of their stage in the journey, they leveraged the
+    services to assist in designing their overall MLOps architecture to ensure project success
+    for years to come. We split one large MLOps pipeline into smaller pipelines that can be
+    developed independently and fed into downstream pipeline steps. This approach is
+    similar to developing microservices for more independent development across your
+    MLOps process for speed of development (while still cooperating effectively as a larger
+    team).`
+  },
+  {
+    company: 'Tempus',
+    logo: 'https://via.placeholder.com/150',
+    summary: `Tempus Labs needed a full redesign of their ML workflows and needed to simplify their
+    existing codebase considerably while adding new capabilities with deeper integration
+    with DVC. Together, we designed a roadmap on how to transition gradually in steps, so
+    that the team did not have to halt production progress at any point.`
+  },
+  {
+    company: 'PACCAR',
+    logo: 'https://via.placeholder.com/150',
+    summary: `The PACCAR team was looking to supplement their ML engineering capacity so that they
+    could keep pushing their model development forward (all while also building out their
+    monorepo architecture). The goal here was to create a template structure for their DVC
+    pipelines to all for more reusability and to remove redundancy in code (making iterations
+    easier and less error-prone).`
+  },
+  {
+    company: 'PlasmaSolve',
+    logo: 'https://via.placeholder.com/150',
+    summary: `The team spent 1 year trying to integrate DVC into their software platform without
+    success, and in 2 weeks we are able to provide a working proof of concept and then in
+    just another 2 weeks we polished and completed the project.`
+  }
+]
 
 const SupportPage: React.FC = () => (
   <>
     <PageContent>
       <HeroContainer className={styles.supportHero}>
-        <h1 className={styles.heading}>
-          Questions, feedback, or just need to get in touch?
+        <h1
+          className={cn(
+            styles.heading,
+            'mx-auto',
+            'font-extrabold text-5xl text-center'
+          )}
+        >
+          Platinum Services for ML Operations
         </h1>
+        <p
+          className={cn(
+            'text-center',
+            'text-xl',
+            'mt-5',
+            'mx-auto',
+            'max-w-2xl',
+            'text-gray-500'
+          )}
+        >
+          Accelerate your business with our world-class data engineering, data
+          science, and project management experts and deliver rapid success in
+          your ML projects.
+        </p>
+
+        <button
+          className={cn(
+            'mt-10',
+            'mx-auto',
+            'block',
+            'px-6',
+            'py-2',
+            'rounded-md',
+            'text-lg',
+            'font-semibold',
+            'text-blue',
+            'border-2',
+            'border-blue',
+            'hover:bg-blue',
+            'hover:text-white'
+          )}
+        >
+          Request a quote
+        </button>
       </HeroContainer>
       <LayoutWidthContainer>
-        <div className={styles.features}>
-          <div className={cn(styles.feature, styles.chat)}>
-            <div className={styles.featureHeader}>
-              <div className={styles.featureIcon} />
-              <h3 className={styles.featureName}>Slack-like Chat</h3>
+        <div className="max-w-2xl mx-auto text-center">
+          <blockquote className="mt-14 italic text-lg">
+            <p className="before:content-['\201C'] after:content-['\201D']">
+              I can confidently say that DVC AI&apos;s Platinum Engineering
+              Services has been an invaluable asset to our team. Their expert
+              knowledge and hands-on keyboard support enabled us to quickly
+              scale our ML operations and unlock value from the business. Their
+              team has been a pleasure to partner with. I highly recommend their
+              services!
+            </p>
+            <footer className="mt-3">
+              <cite className="font-semibold">
+                - Head of MLOps, Fortune 50 Retailer
+              </cite>
+            </footer>
+          </blockquote>
+          <hr className="mt-14 border-t border-gray-200" />
+
+          <h2 className="mt-14 text-3xl font-bold">How We Can Help</h2>
+          <p className="mt-6 text-xl text-gray-500">
+            Successful ML projects are covered in challenges, roadblocks, and
+            problems --- and we are here to address them with industry best
+            practices
+          </p>
+        </div>
+        <div className="p-4 mt-14 grid gap-8 grid-cols-1 md:grid-cols-2">
+          {services.map((service, index) => (
+            <div key={index}>
+              <h3 className="text-2xl font-medium">
+                {service.icon && (
+                  <service.icon
+                    size={48}
+                    strokeWidth={1.5}
+                    className={cn(
+                      getAccentColor(index),
+                      'inline-block',
+                      'mr-2'
+                    )}
+                  />
+                )}
+                {service.title}
+              </h3>
+              <ul className="mt-4 list-disc list-inside text-lg text-gray-600">
+                {service.points.map((point, index) => (
+                  <li key={index} className={cn(getSpanAccentColor(index))}>
+                    {point}
+                  </li>
+                ))}
+              </ul>
             </div>
-            <div className={styles.featureDescription}>
-              Join data science practitioners in our welcoming{' '}
-              <span className={styles.accent}>DVC Community</span>. It’s the
-              fastest way to get help.
+          ))}
+        </div>
+
+        <div className="mt-14 text-center">
+          <h2 className="text-3xl font-bold">Customer Success Stories</h2>
+          <p className="mt-6 text-xl text-gray-500">
+            Our customers have seen significant improvements in their ML
+            projects. Here are a few stories of how we helped them.
+          </p>
+        </div>
+
+        <div className="mt-14 grid gap-8 grid-cols-1 md:grid-cols-2">
+          {successStories.map((story, index) => (
+            <div key={index} className="p-4 border border-gray-200 rounded-md">
+              <div className="flex items-center justify-center">
+                <img
+                  src={story.logo}
+                  alt={`${story.company} logo`}
+                  className={cn(
+                    'w-24 h-24 rounded-md object-contain',
+                    story.logoClassName
+                  )}
+                />
+              </div>
+              <p className="mt-4 text-lg text-gray-600 text-center">
+                {story.summary}
+              </p>
             </div>
-            <div className={styles.featureFooter}>
-              <Link
-                className={styles.featureButton}
-                href="/chat"
-                target="_blank"
-              >
-                Discord Chat
-              </Link>
-              <ShowOnly on="desktop">
-                <Popover
-                  body={
-                    <iframe
-                      title="Discord Members Online"
-                      src="https://discordapp.com/widget?id=485586884165107732&theme=light"
-                      width="350"
-                      height="500"
-                      frameBorder="0"
-                    />
-                  }
-                  enterExitTransitionDurationMs={200}
-                >
-                  <div className={styles.discordWidget} />
-                </Popover>
-              </ShowOnly>
-            </div>
-          </div>
-          <div className={cn(styles.feature, styles.bugs)}>
-            <div className={styles.featureHeader}>
-              <div className={styles.featureIcon} />
-              <h3 className={styles.featureName}>Bugs & Feature Requests</h3>
-            </div>
-            <div className={styles.featureDescription}>
-              Found an issue or have an idea? Check our GitHub{' '}
-              <span className={styles.accent}>issues tracker</span> to see if
-              there is already a fix or report a new one.
-            </div>
-            <div className={styles.featureFooter}>
-              <Link
-                className={styles.featureButton}
-                href="https://github.com/iterative/dvc/issues"
-                target="_blank"
-              >
-                Open GitHub
-              </Link>
-            </div>
-          </div>
-          <div className={cn(styles.feature, styles.forum)}>
-            <div className={styles.featureHeader}>
-              <div className={styles.featureIcon} />
-              <h3 className={styles.featureName}>Forum</h3>
-            </div>
-            <div className={styles.featureDescription}>
-              Discuss your ideas or{' '}
-              <span className={styles.accent}>best practices</span> in the DVC
-              forum.
-            </div>
-            <div className={styles.featureFooter}>
-              <Link
-                className={styles.featureButton}
-                href="https://discuss.dvc.org"
-                target="_blank"
-              >
-                Go To Forum
-              </Link>
-            </div>
-          </div>
-          <div className={cn(styles.feature, styles.email)}>
-            <div className={styles.featureHeader}>
-              <div className={styles.featureIcon} />
-              <h3 className={styles.featureName}>Email</h3>
-            </div>
-            <div className={styles.featureDescription}>
-              Email us at{' '}
-              <Link className={styles.accent} href="mailto:support@dvc.org">
-                support@dvc.org
-              </Link>
-              . For help with debugging DVC commands, run the commands with the{' '}
-              <span className={styles.accent}>--verbose</span> flag and include
-              the output if possible.
-            </div>
-            <div className={styles.featureFooter}>
-              <Link
-                className={styles.featureButton}
-                href="mailto:support@dvc.org"
-              >
-                Drop Us a Line
-              </Link>
-            </div>
-          </div>
+          ))}
         </div>
       </LayoutWidthContainer>
     </PageContent>
     <PromoSection
-      title="Don't know where to start?"
+      title="Feel free to join the best community of MLOps experts and enthusiasts !"
       buttons={[
-        <Link href="/doc/start" key="get-started">
-          Get Started
+        <Link href="/community" key="get-started">
+          Join Our Community
         </Link>
       ]}
     />
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       colors: {
         gray: { hover: '#40364d', dark: 'rgb(26, 30, 35)' },
         purple: { DEFAULT: '#945dd6' },
-        blue: { DEFAULT: 'rgb(56, 179, 220)' },
+        blue: { DEFAULT: '#13ADC7', hover: '#13a3bd' },
         orange: { DEFAULT: '#F46737' },
         indigo: { DEFAULT: '#4B2E70' },
         dark: { DEFAULT: '#1A1E23' },
