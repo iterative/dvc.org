49b9ad003a5c65e9fac982946b5ab4ffa17b7966
Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>
2024-02-20 18:42:59 +0545

    drop customer success

--- src/components/Support/index.tsx
+++ src/components/Support/index.tsx
@@ -93,61 +93,6 @@ const getSpanAccentColor = (index: number) => {
   return spanColors[index % colors.length]
 }
 
-const successStories = [
-  {
-    company: 'UBS',
-    logo: '/logos/ubs.png',
-    logoClassName: 'invert',
-    summary: `Iterative is a strategic partner for the AI element of UBS’s in-house data platform. Our
-    team is being leveraged to design a standardized ML development environment with full
-    reproducibility and audibility in mind. The project scope went from 6 months to 1 month.`
-  },
-  {
-    company: 'Walmart',
-    logo: '/logos/walmart.svg',
-    logoClassName: 'w-48 object-cover',
-    summary: `In addition the DVCx platform being instrumental in Walmart’s GenAI pipelines, Walmart
-    leverages Iterative Platinum Services to integrate their entire MLOps pipeline and get
-    support for their specific ML use cases to accelerate adoption and development speed.`
-  },
-  {
-    company: 'Omnisens',
-    logo: 'https://via.placeholder.com/150',
-    summary: `Omnisens was earlier in their ML journey when they started to engage the Iterative
-    Platinum Services team. Because of their stage in the journey, they leveraged the
-    services to assist in designing their overall MLOps architecture to ensure project success
-    for years to come. We split one large MLOps pipeline into smaller pipelines that can be
-    developed independently and fed into downstream pipeline steps. This approach is
-    similar to developing microservices for more independent development across your
-    MLOps process for speed of development (while still cooperating effectively as a larger
-    team).`
-  },
-  {
-    company: 'Tempus',
-    logo: 'https://via.placeholder.com/150',
-    summary: `Tempus Labs needed a full redesign of their ML workflows and needed to simplify their
-    existing codebase considerably while adding new capabilities with deeper integration
-    with DVC. Together, we designed a roadmap on how to transition gradually in steps, so
-    that the team did not have to halt production progress at any point.`
-  },
-  {
-    company: 'PACCAR',
-    logo: 'https://via.placeholder.com/150',
-    summary: `The PACCAR team was looking to supplement their ML engineering capacity so that they
-    could keep pushing their model development forward (all while also building out their
-    monorepo architecture). The goal here was to create a template structure for their DVC
-    pipelines to all for more reusability and to remove redundancy in code (making iterations
-    easier and less error-prone).`
-  },
-  {
-    company: 'PlasmaSolve',
-    logo: 'https://via.placeholder.com/150',
-    summary: `The team spent 1 year trying to integrate DVC into their software platform without
-    success, and in 2 weeks we are able to provide a working proof of concept and then in
-    just another 2 weeks we polished and completed the project.`
-  }
-]
-
 const SupportPage: React.FC = () => (
   <>
     <PageContent>
@@ -246,34 +191,6 @@ const SupportPage: React.FC = () => (
                   </li>
                 ))}
               </ul>
-            </div>
-          ))}
-        </div>
-
-        <div className="mt-14 text-center">
-          <h2 className="text-3xl font-bold">Customer Success Stories</h2>
-          <p className="mt-6 text-xl text-gray-500">
-            Our customers have seen significant improvements in their ML
-            projects. Here are a few stories of how we helped them.
-          </p>
-        </div>
-
-        <div className="mt-14 grid gap-8 grid-cols-1 md:grid-cols-2">
-          {successStories.map((story, index) => (
-            <div key={index} className="p-4 border border-gray-200 rounded-md">
-              <div className="flex items-center justify-center">
-                <img
-                  src={story.logo}
-                  alt={`${story.company} logo`}
-                  className={cn(
-                    'w-24 h-24 rounded-md object-contain',
-                    story.logoClassName
-                  )}
-                />
-              </div>
-              <p className="mt-4 text-lg text-gray-600 text-center">
-                {story.summary}
-              </p>
             </div>
           ))}
         </div>
