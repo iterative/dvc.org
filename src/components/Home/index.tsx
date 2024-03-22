fe2d4d0661d2221fdd7255f3339269ab89037d50
Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>
2024-03-22 15:19:18 +0545

    import is no longer needed

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,7 +6,6 @@ import SubscribeSection from '../SubscribeSection'
 
 import CompanySlider from './LogosSlider'
 import Hero from './Hero'
-import Alert from './Alert'
 
 const Home: React.FC = () => {
   return (
