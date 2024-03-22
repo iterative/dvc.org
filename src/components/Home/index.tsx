28a0ae73355ed73f149938802a9a1fc20a19253a
Jenifer De Figueiredo <jeny.defigueiredo@gmail.com>
2024-03-22 03:37:41 -0700

    removing banner temporarily (#5188)
    * removing banner temporarily
    
    * keep Alert component and remove banner from home page
    
    * import is no longer needed
    
    ---------
    
    Co-authored-by: Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,13 +6,11 @@ import SubscribeSection from '../SubscribeSection'
 
 import CompanySlider from './LogosSlider'
 import Hero from './Hero'
-import Alert from './Alert'
 
 const Home: React.FC = () => {
   return (
     <>
       <WhatsNewModal />
-      <Alert />
       <Hero />
       <CompanySlider />
       <SubscribeSection />
