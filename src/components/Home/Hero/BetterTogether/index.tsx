327086eb9f2b3a39b11a8bf445afe508b5e83e93
Daniel Sauble <djsauble@gmail.com>
2024-07-16 11:26:45 -0700

    Replace occurrences of 'DataChain' with 'DVCx' in the website (#5281)
    * Replace occurrences of 'DataChain' with 'DVCx' in the website
    
    * Remove 'data chain' from the hero section

--- src/components/Home/Hero/BetterTogether/index.tsx
+++ src/components/Home/Hero/BetterTogether/index.tsx
@@ -5,7 +5,7 @@ const BetterTogether = () => {
   return (
     <HeroContainer className="py-14 bg-blue-100">
       <h2 className={cn('text-center text-3xl font-medium')}>
-        Datachain and DVC: Better Together
+        DVCx and DVC: Better Together
       </h2>
       <p className={cn('mt-8', 'text-xl font-normal')}>
         Build the datasets you need without modifying your data sources. Create
