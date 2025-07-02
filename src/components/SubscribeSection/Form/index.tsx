c977135a86ae01889edb014a3edc91a22130cd1c
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-07-02 18:54:26 +0545

    useEffect

--- src/components/SubscribeSection/Form/index.tsx
+++ src/components/SubscribeSection/Form/index.tsx
@@ -6,25 +6,35 @@ import { logEvent } from '@dvcorg/gatsby-theme/src/utils/front/plausible'
 import './hubspot.css'
 
 const Form: React.FC = () => {
-  const [loaded, setLoaded] = useState(false)
+  const [scriptLoaded, setScriptLoaded] = useState(false)
+  const [formLoaded, setFormLoaded] = useState(false)
   const [error, setError] = useState(false)
 
   useEffect(() => {
-    const onSubmit = (event: MessageEvent) => {
-      if (
-        event.data.type === 'hsFormCallback' &&
-        event.data.eventName === 'onFormSubmitted'
-      ) {
-        logEvent('Subscribe Form')
-      }
+    if (scriptLoaded) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ;(window as any).hbspt.forms.create({
+        portalId: '8040338',
+        formId: '2912f9fd-d4db-40c2-9f57-b9c30fa57ef2',
+        region: 'na1',
+        target: '#email-subscription-form-hbspot',
+        cssRequired: false,
+        cssClass: 'email-subscription-form',
+        submitText: 'Subscribe',
+        submitButtonClass: 'email-subscribe-btn',
+        errorClass: 'email-subscribe-error',
+        errorMessageClass: 'email-subscribe-error-message',
+        onFormReady: () => setFormLoaded(true),
+        onFormSubmitted: () => {
+          logEvent('Subscribe Form')
+        }
+      })
     }
-    window.addEventListener('message', onSubmit)
-    return () => window.removeEventListener('message', onSubmit)
-  }, [])
+  }, [scriptLoaded])
 
   return (
     <>
-      {!loaded && !error && (
+      {!formLoaded && !error && (
         <div className="hubspot-form-loading-message hubspot-form-message">
           Loading form...
         </div>
@@ -37,25 +47,9 @@ const Form: React.FC = () => {
       <Script
         type="text/javascript"
         src="//js.hsforms.net/forms/embed/v2.js"
-        onLoad={() => setLoaded(true)}
+        onLoad={() => setScriptLoaded(true)}
         onError={() => setError(true)}
       />
-      {loaded && (
-        <Script>
-          {`hbspt.forms.create({
-        portalId: "8040338",
-        formId: "2912f9fd-d4db-40c2-9f57-b9c30fa57ef2",
-        region: "na1",
-        target: '#email-subscription-form-hbspot',
-        cssRequired: false,
-        cssClass: 'email-subscription-form',
-        submitText: 'Subscribe',
-        submitButtonClass: 'email-subscribe-btn',
-        errorClass: 'email-subscribe-error',
-        errorMessageClass: 'email-subscribe-error-message',
-      })`}
-        </Script>
-      )}
       <div id="email-subscription-form-hbspot"></div>
     </>
   )
