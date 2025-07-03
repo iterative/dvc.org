37cdca5ae261e3e7f6d888308e273568a3bab47a
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-07-03 10:58:35 +0545

    use dynamically generated  css and remove flickering by using onBeforeFormInit callback.

--- src/components/SubscribeSection/Form/index.tsx
+++ src/components/SubscribeSection/Form/index.tsx
@@ -3,7 +3,59 @@ import { useEffect, useState } from 'react'
 
 import { logEvent } from '@dvcorg/gatsby-theme/src/utils/front/plausible'
 
-import './hubspot.css'
+import * as styles from './styles.module.css'
+
+const supportRedirect = false
+
+interface SubmissionValue {
+  name: 'email'
+  value: string
+}
+
+interface HubSpotFormConfig {
+  // https://developers.hubspot.com/docs/reference/cms/forms/legacy-forms
+  portalId: string
+  formId: string
+  region: string
+  target: string
+  redirectUrl?: string
+  inlineMessage?: string
+  pageId?: number | string
+  cssRequired?: boolean
+  cssClass?: string
+  css?: string
+  submitText?: string
+  submitButtonClass?: string
+  errorClass?: string
+  errorMessageClass?: string
+  locale?: string
+  translations?: object
+  manuallyBlockedEmailDomain?: string[]
+  formInstanceId?: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onBeforeFormInit?: (ctx: any) => void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onFormReady?: ($form: any) => void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onFormSubmit?: ($form: any) => void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onBeforeFormSubmit?: ($form: any, submissionValues: SubmissionValue[]) => void
+  onFormSubmitted?: (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    $form: any,
+    data: { redirectUrl?: string; submissionValues: SubmissionValue[] }
+  ) => void
+}
+
+declare global {
+  interface Window {
+    hbspt?: {
+      forms?: {
+        create: (config: HubSpotFormConfig) => void
+      }
+    }
+  }
+}
 
 const Form: React.FC = () => {
   const [scriptLoaded, setScriptLoaded] = useState(false)
@@ -13,21 +65,27 @@ const Form: React.FC = () => {
   useEffect(() => {
     if (scriptLoaded) {
       try {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ;(window as any).hbspt.forms.create({
+        if (!window.hbspt?.forms?.create) {
+          throw new Error('HubSpot forms API not available')
+        }
+
+        window.hbspt.forms.create({
           portalId: '8040338',
           formId: '2912f9fd-d4db-40c2-9f57-b9c30fa57ef2',
           region: 'na1',
-          target: '#email-subscription-form-hbspot',
+          target: `#${styles.emailSubscriptionFormHubspotTarget}`,
           cssRequired: false,
-          cssClass: 'email-subscription-form',
+          cssClass: styles.emailSubscriptionForm,
           submitText: 'Subscribe',
-          submitButtonClass: 'email-subscribe-btn',
+          submitButtonClass: styles.emailSubscribeBtn,
           errorClass: 'email-subscribe-error',
-          errorMessageClass: 'email-subscribe-error-message',
-          onFormReady: () => setFormLoaded(true),
-          onFormSubmitted: () => {
+          errorMessageClass: styles.emailSubscribeErrorMessage,
+          onBeforeFormInit: () => setFormLoaded(true),
+          onFormSubmitted: (_, data) => {
             logEvent('Subscribe Form')
+            if (supportRedirect && data.redirectUrl) {
+              window.location.href = data.redirectUrl
+            }
           }
         })
       } catch (e) {
@@ -40,12 +98,12 @@ const Form: React.FC = () => {
   return (
     <>
       {!formLoaded && !error && (
-        <div className="hubspot-form-loading-message hubspot-form-message">
-          Loading form...
-        </div>
+        <div className={styles.hubspotFormMessage}>&nbsp;</div>
       )}
       {error && (
-        <div className="hubspot-form-loading-failed-message hubspot-form-message">
+        <div
+          className={`${styles.hubspotFormLoadingFailedMessage} ${styles.hubspotFormMessage}`}
+        >
           There was an error loading the form. Please try again later.
         </div>
       )}
@@ -55,7 +113,7 @@ const Form: React.FC = () => {
         onLoad={() => setScriptLoaded(true)}
         onError={() => setError(true)}
       />
-      <div id="email-subscription-form-hbspot"></div>
+      <div id={styles.emailSubscriptionFormHubspotTarget}></div>
     </>
   )
 }
