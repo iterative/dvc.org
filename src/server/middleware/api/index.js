d1978aafad4bb04391ad550283e67685ef820eb9
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-07-28 16:59:34 +0545

    api: remove /dvcx/waitlist

--- src/server/middleware/api/index.js
+++ src/server/middleware/api/index.js
@@ -3,11 +3,9 @@ const routes = require('express').Router()
 const comments = require('./comments')
 const discourse = require('./discourse')
 const { issues, stars } = require('./github')
-const subscribe = require('./subscribe')
 
 routes.get('/comments', comments)
 routes.get('/discourse', discourse)
-routes.use('/subscribe', subscribe)
 routes.get('/github/issues', issues)
 routes.get('/github/stars', stars)
 
