55866f43bb514e6fd2c6ea8916d6cbfa31e3053f
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-11-15 13:29:27 +0545

    remove not-existing imports

--- src/gatsby/models.js
+++ src/gatsby/models.js
@@ -1,7 +1,6 @@
-const github = require('./models/github')
 const imageSourcePaths = require('./models/image-source-paths')
 const pruneCache = require('./models/prune-cache')
 
-const models = [imageSourcePaths, github, pruneCache]
+const models = [imageSourcePaths, pruneCache]
 
 module.exports = models
