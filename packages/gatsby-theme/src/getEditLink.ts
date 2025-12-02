a8ff42f278f784d9137a827213565ce43e1eddbd
skshetry <18718008+skshetry@users.noreply.github.com>
2025-12-02 16:11:55 +0545

    fix editLink

--- packages/gatsby-theme/src/getEditLink.ts
+++ packages/gatsby-theme/src/getEditLink.ts
@@ -1,5 +1,5 @@
 import { externalUrls } from '../consts'
 
 const getEditLink = (sourcePath: string): string =>
-  `${externalUrls.dvcRepo}/blob/main/content${sourcePath}`
+  `${externalUrls.dvcOrgRepo}/blob/main/content${sourcePath}`
 export default getEditLink
